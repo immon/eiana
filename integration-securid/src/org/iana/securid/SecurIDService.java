e850f6301b2d11f7e9db1f75366a897b7ae13783
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-02-28 13:46:31 +0000

    Comments added.

--- integration-securid/src/org/iana/securid/SecurIDService.java
+++ integration-securid/src/org/iana/securid/SecurIDService.java
@@ -1,6 +1,11 @@
 package org.iana.securid;
 
 /**
+ * <p>
+ * This interface is a boundary interface dedicated to establish a common contract between the application
+ * and SecurID authentication server.
+ * </p>
+ *
  * @author Patrycja Wegrzynowicz
  */
 public interface SecurIDService {
