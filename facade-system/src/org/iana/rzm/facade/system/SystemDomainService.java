bd7112f0e43720ab086e5d866fc8ca89286d1f43
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-02 16:15:51 +0000

    UserInvalidatedException refactoring, plus a new exception added.

--- facade-system/src/org/iana/rzm/facade/system/SystemDomainService.java
+++ facade-system/src/org/iana/rzm/facade/system/SystemDomainService.java
@@ -3,32 +3,43 @@ package org.iana.rzm.facade.system;
 import org.iana.rzm.facade.auth.AccessDeniedException;
 import org.iana.rzm.common.exceptions.InfrastructureException;
 import org.iana.rzm.facade.common.RZMStatefulService;
+import org.iana.rzm.facade.common.NoObjectFoundException;
 
 import java.util.List;
 
 /**
- * <p>This is a stateful domain facade service providing the basic operations on domains for the RZM end-users.</p>
+ * <p>This is a stateful domain facade service providing the basic operations on domains for the eIANA RZM end-users.</p>
+ *
+ * <p>Note that this service is stateful and designed to be called in a user context. Thus all the methods
+ * check whether the user has been granted respective permissions. Often the permission checks are performed
+ * in a context of a given resource, like, a domain object.</p>
  *
  * @author Patrycja Wegrzynowicz
  */
 public interface SystemDomainService extends RZMStatefulService {
 
     /**
-     * Returns a domain object with a given id.
+     * <p>Returns a domain object with a given id. It checks whether the user has been granted access
+     * to the specified domain. For example, IANA stuff is allowed to access all domains while system users
+     * are permitted to access only those domain objects for which they are in one of the roles: AC, TC, SO.</p>
      *
-     * @param id
-     * @return
-     * @throws org.iana.rzm.facade.auth.AccessDeniedException - user is not authenticated or has no permission to access a given domain object.
-     * @throws InfrastructureException
+     * @param id the identifier of the domain to be found
+     * @return the domain object if found and the user is allowed to access it
+     * @throws AccessDeniedException when the user is not allowed to access this method or the given domain object
+     * @throws NoObjectFoundException when the domain is not found
+     * @throws InfrastructureException when a low-level exception happened
      */
-    IDomainVO getDomain(long id) throws AccessDeniedException, InfrastructureException;
+    IDomainVO getDomain(long id) throws AccessDeniedException, InfrastructureException, NoObjectFoundException;
 
     /**
-     * Returns a list of the domain objects for which a specified user is in one of the roles: AC, TC, SO.
+     * <p>Returns a list of the domains objects that the provided user is granted to access. For example,
+     * IANA stuff is allowed to access all domains thus this method returns all existing domain objects. While
+     * system users are allowed to access only the domains for which they are in one of the roles: AC, TC, SO, thus
+     * for them the returned list is limited only to those domains that they manage/administer at the moment of time.</p>
      *
-     * @return a list of the domains managed/administered by a given user, an empty list if no domains found.
-     * @throws org.iana.rzm.facade.auth.AccessDeniedException - user is not authenticated.
-     * @throws InfrastructureException
+     * @return the list of the domains managed/administered by a given user, an empty list if no domains found.
+     * @throws AccessDeniedException when the user is not allowed to access this method
+     * @throws InfrastructureException when a low-level exception happened
      */
-    List<SimpleDomainVO> findDomains() throws AccessDeniedException, InfrastructureException;
+    List<SimpleDomainVO> findUserDomains() throws AccessDeniedException, InfrastructureException;
 }
