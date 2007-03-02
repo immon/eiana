938a8e136fb2e473fd0676b46c93192c6f5bd49a
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-02 17:35:06 +0000

    eiana-users test packages refactored

---
+++ eiana-users/test/hibernate/org/iana/rzm/user/hibernate/test/common/HibernateTest.java
@@ -0,0 +1,37 @@
+package org.iana.rzm.user.hibernate.test.common;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ * @author Jakub Laszkiewicz
+ */
+abstract public class HibernateTest {
+    protected Session session;
+    protected Transaction tx = null;
+    protected SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+
+    protected void begin() {
+        session = sessionFactory.openSession();
+        tx = session.beginTransaction();
+    }
+
+    protected void close() {
+        if (tx != null) {
+            tx.commit();
+            tx = null;
+        }
+        session.close();
+    }
+
+    protected void beginTx() {
+        tx = session.beginTransaction();
+    }
+
+    protected void closeTx() {
+        tx.commit();
+        tx = null;
+    }
+}
