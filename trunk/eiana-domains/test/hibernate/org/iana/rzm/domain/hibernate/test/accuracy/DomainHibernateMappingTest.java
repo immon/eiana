e1d5975b74eb49293d023ece32a59e7f128dc2ad
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-02 17:38:16 +0000

    eiana-domains test packages refactored

---
+++ trunk/eiana-domains/test/hibernate/org/iana/rzm/domain/hibernate/test/accuracy/DomainHibernateMappingTest.java
@@ -0,0 +1,48 @@
+package org.iana.rzm.domain.hibernate.test.accuracy;
+
+import org.iana.rzm.domain.Domain;
+import org.iana.rzm.domain.Contact;
+import org.iana.rzm.domain.Host;
+import org.iana.rzm.domain.hibernate.test.common.HibernateMappingUnitTest;
+import org.iana.rzm.domain.hibernate.test.common.HibernateMappingTestUtil;
+import org.testng.annotations.Test;
+
+import java.io.Serializable;
+import java.net.URL;
+
+/**
+ * @author Jakub Laszkiewicz
+ */
+public class DomainHibernateMappingTest extends HibernateMappingUnitTest<Domain> {
+    protected Domain create() throws Exception {
+        return HibernateMappingTestUtil.setupDomain(new Domain("iana.org"));
+    }
+
+    protected Domain change(Domain o) throws Exception {
+        o.setName("changed-iana.org");
+        o.setRegistryUrl(new URL("http://changed-registry.com"));
+        o.setSpecialInstructions("changed special instructions");
+        o.setState(Domain.State.OPERATIONS_PENDING);
+        o.setStatus(Domain.Status.ACTIVE);
+        o.setSupportingOrg(HibernateMappingTestUtil.setupContact(new Contact(), "changed supporting", true));
+        o.setWhoisServer("changed.whoid.server");
+        o.removeAdminContact(o.getAdminContacts().iterator().next());
+        o.addAdminContact(HibernateMappingTestUtil.setupContact(new Contact(), "admin3", true));
+        o.removeBreakpoint(Domain.Breakpoint.AC_CHANGE_EXT_REVIEW);
+        o.addBreakpoint(Domain.Breakpoint.NS_CHANGE_EXT_REVIEW);
+        o.removeNameServer(o.getNameServers().iterator().next());
+        o.addNameServer(HibernateMappingTestUtil.setupHost(new Host("ns3." + o.getName())));
+        o.removeTechContact(o.getTechContacts().iterator().next());
+        o.addTechContact(HibernateMappingTestUtil.setupContact(new Contact(), "tech3", true));
+        return o;
+    }
+
+    protected Serializable getId(Domain o) {
+        return o.getObjId();
+    }
+
+    @Test(groups = {"hibernate", "eiana-domains"})
+    public void testDomain() throws Exception {
+        super.test();
+    }
+}
