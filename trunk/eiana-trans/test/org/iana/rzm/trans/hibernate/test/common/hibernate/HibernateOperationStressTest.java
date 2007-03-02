40bc02891e42491c181d2249f31ebc78b4ece0e6
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-02 17:43:17 +0000

    eiana-trans test packages refactored

---
+++ trunk/eiana-trans/test/org/iana/rzm/trans/hibernate/test/common/hibernate/HibernateOperationStressTest.java
@@ -0,0 +1,35 @@
+package org.iana.rzm.trans.hibernate.test.common.hibernate;
+
+import java.util.List;
+
+/**
+ * @author Jakub Laszkiewicz
+ */
+public abstract class HibernateOperationStressTest extends org.iana.rzm.domain.hibernate.test.common.HibernateTest {
+    abstract protected void operation(Object o) throws Exception;
+    abstract protected List getList() throws Exception;
+
+    protected void oneTransaction() throws Exception {
+        begin();
+        for (Object o : getList()) operation(o);
+        close();
+    }
+
+    protected void manyTransactions() throws Exception {
+        begin();
+        for (Object o : getList()) {
+            beginTx();
+            operation(o);
+            closeTx();
+        }
+        close();
+    }
+
+    protected void manySessions() throws Exception {
+        for (Object o : getList()) {
+            begin();
+            operation(o);
+            close();
+        }
+    }
+}
