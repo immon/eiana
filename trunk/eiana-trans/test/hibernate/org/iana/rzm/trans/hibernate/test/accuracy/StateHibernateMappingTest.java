7b005184b89867726f33ec9a26640e35b2b2dc29
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-02 17:48:54 +0000

    eiana-trans test packages refactored

---
+++ trunk/eiana-trans/test/hibernate/org/iana/rzm/trans/hibernate/test/accuracy/StateHibernateMappingTest.java
@@ -0,0 +1,39 @@
+package org.iana.rzm.trans.hibernate.test.accuracy;
+
+import org.iana.rzm.trans.TransactionState;
+import org.iana.rzm.trans.StateTransition;
+import org.iana.rzm.trans.hibernate.test.common.HibernateMappingTestUtil;
+import org.iana.rzm.trans.hibernate.test.common.HibernateMappingUnitTest;
+import org.testng.annotations.Test;
+
+import java.io.Serializable;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * @author Jakub Laszkiewicz
+ */
+public class StateHibernateMappingTest extends HibernateMappingUnitTest<TransactionState> {
+    protected TransactionState create() throws Exception {
+        Set<StateTransition> transitions = new HashSet<StateTransition>();
+        transitions.add(new StateTransition("1st transition"));
+        transitions.add(new StateTransition("2nd transition"));
+        return HibernateMappingTestUtil.setupState(new TransactionState(), TransactionState.Name.ADMIN_CLOSE, transitions);
+    }
+
+    protected TransactionState change(TransactionState o) throws Exception {
+        Set<StateTransition> transitions = o.getAvailableTransitions();
+        transitions.remove(transitions.iterator().next());
+        transitions.add(new StateTransition("3rd transition"));
+        return HibernateMappingTestUtil.setupState(o, TransactionState.Name.COMPLETED, transitions);
+    }
+
+    protected Serializable getId(TransactionState o) {
+        return o.getObjId();
+    }
+
+    @Test(groups = {"hibernate", "eiana-trans"})
+    public void testState() throws Exception {
+        super.test();
+    }
+}
---
+++ trunk/eiana-trans/test/hibernate/org/iana/rzm/trans/hibernate/test/stress/TransactionDeletionHibernateTest.java
@@ -0,0 +1,25 @@
+package org.iana.rzm.trans.hibernate.test.stress;
+
+import org.iana.rzm.trans.hibernate.test.common.HibernateOperationStressTest;
+import org.iana.rzm.trans.Transaction;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * @author Jakub Laszkiewicz
+ */
+public class TransactionDeletionHibernateTest extends HibernateOperationStressTest {
+    protected void operation(Object o) throws Exception {
+        session.delete(o);
+    }
+
+    protected List getList() throws Exception {
+        return session.createCriteria(Transaction.class).list();
+    }
+
+    @Test
+    public void oneTransaction() throws Exception {
+        super.oneTransaction();
+    }
+}
