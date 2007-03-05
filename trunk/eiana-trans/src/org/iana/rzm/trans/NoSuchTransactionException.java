aeb05d69ba098fb46c61559efd83ed8537192290
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-05 17:16:42 +0000

    Transaction refactoring.

--- trunk/eiana-trans/src/org/iana/rzm/trans/NoSuchTransactionException.java
+++ trunk/eiana-trans/src/org/iana/rzm/trans/NoSuchTransactionException.java
@@ -4,4 +4,14 @@ package org.iana.rzm.trans;
  * @author Patrycja Wegrzynowicz
  */
 public class NoSuchTransactionException extends TransactionException {
+
+    private long id;
+
+    public NoSuchTransactionException(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
--- trunk/eiana-trans/src/org/iana/rzm/trans/TransactionManagerBean.java
+++ trunk/eiana-trans/src/org/iana/rzm/trans/TransactionManagerBean.java
@@ -18,24 +18,25 @@ public class TransactionManagerBean implements TransactionManager {
         this.context = context;
     }
 
-    public Transaction get(long id) throws TransactionException {
+    public Transaction get(long id) throws NoSuchTransactionException {
         GraphSession graphSession = context.getGraphSession();
         List processInstances =
                 graphSession.findProcessInstances(id);
+        if (processInstances == null || processInstances.isEmpty()) throw new NoSuchTransactionException(id);
         ProcessInstance pi = (ProcessInstance) processInstances.get(0);
         Transaction transaction = new Transaction(pi.getProcessDefinition().getName());
         return transaction;
     }
 
-    public List<Transaction> create(Domain domain) throws TransactionException {
+    public Transaction create(Domain domain) {
         return null;
     }
 
-    public List<Transaction> findAll() throws TransactionException {
+    public List<Transaction> findAll() {
         return null;
     }
 
-    public List<Transaction> find(TransactionCriteria criteria) throws TransactionException {
+    public List<Transaction> find(TransactionCriteria criteria) {
         return null;
     }
 }
