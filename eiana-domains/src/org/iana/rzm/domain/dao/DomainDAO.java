580639af8e003c992d857f1bc3a5060aeb825d1a
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-06 12:40:43 +0000

    Spring based dao.

---
+++ eiana-domains/src/org/iana/rzm/domain/dao/DomainDAO.java
@@ -0,0 +1,16 @@
+package org.iana.rzm.domain.dao;
+
+import org.iana.rzm.domain.Domain;
+
+/**
+ *
+ * @author Patrycja Wegrzynowicz
+ */
+public interface DomainDAO {
+
+    public Domain get(long id);
+
+    public Domain get(String name);
+
+    public void create(Domain domain);
+}
