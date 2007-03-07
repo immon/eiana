e91babee40f024c8ef884b9f761d806f9aeb693d
marcinzzz <marcinzzz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-07 14:04:34 +0000

    feature: added initial version of UserDAO

---
+++ trunk/eiana-users/src/org/iana/rzm/user/dao/HibernateUserDAO.java
@@ -0,0 +1,38 @@
+package org.iana.rzm.user.dao;
+
+import org.iana.rzm.user.User;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import java.util.Iterator;
+
+/**
+ * org.iana.rzm.user.dao.HibernateUserDAO
+ *
+ * @author Marcin Zajaczkowski
+ *
+ * todo Generic methods (get, create, delete) need to be implemented in every specific DAO. That could be (probably) simplified.
+ * For example by something like that:
+ * interface DataAccessObject<T> {}
+ * interface UserDAO extends DataAccessObject<User> {}
+ * class HibernateUserDAO extends HibernateDAO<User> implements UserDAO {}
+ *
+ * class abstract HibernateDAO<T> extends HibernateDaoSupport implements DataAccessObject<T> {}
+ */
+public class HibernateUserDAO extends HibernateDaoSupport implements UserDAO {
+
+    public User get(long id) {
+        return (User) getHibernateTemplate().get(User.class, id);
+    }
+
+    public void create(User user) {
+        getHibernateTemplate().save(user);
+    }
+
+    public void update(User user) {
+        getHibernateTemplate().update(user);
+    }
+
+    public void delete(User user) {
+        getHibernateTemplate().delete(user);
+    }
+}
