a91404de3e00e30825a6e90b140103092ae1f439
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-08 17:29:42 +0000

    Exception removed - the only checked exceptions will be logic excepiton while iinfrastructure exceptions are going to be runtime ones.

--- trunk/eiana-users/src/org/iana/rzm/user/UserManager.java
+++ trunk/eiana-users/src/org/iana/rzm/user/UserManager.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * This interface provides user management functionality.
  *
- * @author Patrycja Wegrzynowicz
+ * @author Patrycja Wegrzynooiwicz
  */
 public interface UserManager {
 
