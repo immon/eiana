e79a6ebde3db58bac56006e0a2279c007a481a36
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-08 17:39:46 +0000

    added GuardedSystemDomainService tests

---
+++ trunk/facade-system/test/bean/org/iana/rzm/system/accuracy/GuardedSystemDomainServiceTest.java
@@ -0,0 +1,79 @@
+package org.iana.rzm.system.accuracy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.iana.rzm.facade.system.*;
+import org.iana.rzm.facade.auth.AuthenticatedUser;
+import org.iana.rzm.facade.auth.AuthenticationService;
+import org.iana.rzm.facade.auth.TestAuthenticatedUser;
+import org.iana.rzm.facade.user.UserVO;
+import org.iana.rzm.facade.user.RoleVO;
+import org.iana.rzm.facade.user.SystemRoleVO;
+import org.iana.rzm.user.dao.UserDAO;
+import org.iana.rzm.user.SystemUser;
+import org.iana.rzm.domain.dao.DomainDAO;
+import org.iana.rzm.domain.Domain;
+import org.iana.rzm.common.exceptions.InvalidNameException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * @author Piotr Tkaczyk
+ */
+
+@Test(groups = {"service", "facade-system"})
+public class GuardedSystemDomainServiceTest {
+    private SystemDomainService gsds;
+    private long domainId;
+
+    @BeforeClass
+    public void init() {
+        gsds = (SystemDomainService) new ClassPathXmlApplicationContext("spring-facade-system.xml").getBean("GuardedSystemDomainService");
+        try {
+            DomainDAO domainDAO = (DomainDAO) new ClassPathXmlApplicationContext("spring-facade-system.xml").getBean("domainDAO");
+            Domain domainCreated = new Domain("iana.org");
+            domainCreated.setWhoisServer("whoIsServer");
+            domainDAO.create(domainCreated);
+            domainId = domainCreated.getObjId();
+        } catch (InvalidNameException e) {
+           //
+        }
+    }
+
+    @Test
+    public void testGetDomainById() throws Exception {
+        TestAuthenticatedUser testAuthUser = new TestAuthenticatedUser(generateUser());
+        gsds.setUser(testAuthUser.getAuthUser());
+        DomainVO domainVO = (DomainVO) gsds.getDomain(domainId);
+        assert domainVO.getName().equals("iana.org");
+    }
+    
+    @Test
+    public void testGetDomainByUserName() throws Exception {
+        TestAuthenticatedUser testAuthUser = new TestAuthenticatedUser(generateUser());
+        gsds.setUser(testAuthUser.getAuthUser());
+        List<SimpleDomainVO> list = gsds.findUserDomains("test");
+        for(Iterator iterator = list.iterator(); iterator.hasNext();) {
+            SimpleDomainVO simpleDomainVO = (SimpleDomainVO) iterator.next();
+            System.out.print("User Domain retrived by findUserDomains: " + simpleDomainVO.getName() + "\n");
+        }
+    }
+
+    private UserVO generateUser() {
+        UserVO user = new UserVO();
+        user.setFirstName("Geordi");
+        user.setLastName("LaForge");
+        user.setUserName("test");
+        SystemRoleVO role = new SystemRoleVO();
+        role.setName("iana.org");
+        role.setType(SystemRoleVO.SystemType.AC);
+        user.addRole(role);
+        role = new SystemRoleVO();
+        role.setName("iana.org");
+        role.setType(SystemRoleVO.SystemType.TC);
+        user.addRole(role);
+        return user;
+    }
+}
---
+++ trunk/facade-system/test/bean/org/iana/rzm/system/accuracy/TestSystemUserManager.java
@@ -0,0 +1,66 @@
+package org.iana.rzm.system.accuracy;
+
+/**
+ * @author Piotr Tkaczyk
+ */
+
+import org.iana.rzm.user.*;
+import org.iana.rzm.common.exceptions.InvalidNameException;
+
+import java.util.List;
+
+public class TestSystemUserManager implements UserManager {
+
+    private final RZMUser user;
+
+    public TestSystemUserManager() {
+        this.user = createSystemUser();
+    }
+
+    public RZMUser get(long id) {
+        return user;
+    }
+
+    public RZMUser get(String loginName) {
+        //"facade-common-test" could be a constant
+        if ("test".equals(loginName)) {
+            return user;
+        } else {
+            return null;
+        }
+    }
+
+    public void create(RZMUser user) {
+        throw new IllegalStateException("Not implemented yet.");
+    }
+
+    public List<RZMUser> findAll() {
+        throw new IllegalStateException("Not implemented yet.");
+    }
+
+    public List<RZMUser> find(UserCriteria criteria) {
+        throw new IllegalStateException("Not implemented yet.");
+    }
+
+    private SystemUser createSystemUser() {
+        SystemUser userCreated = new SystemUser();
+        userCreated.setFirstName("Geordi");
+        userCreated.setLastName("LaForge");
+        userCreated.setLoginName("test");
+        userCreated.setPassword("test");
+        userCreated.setObjId(1L);
+        try {
+            Role role = new Role();
+            role.setName("iana.org");
+            role.setType(Role.Type.TC);
+            userCreated.addRole(role);
+            role = new Role();
+            role.setName("iana.org");
+            role.setType(Role.Type.AC);
+            userCreated.addRole(role);
+        } catch (InvalidNameException e) {
+            //
+        }
+        return userCreated;
+    }
+}
