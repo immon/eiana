18716ba96d39a0558c0c1eabb50fde514015c366
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-08 17:45:41 +0000

    added only for test purpose

---
+++ trunk/facade-system/src/org/iana/rzm/facade/auth/TestAuthenticatedUser.java
@@ -0,0 +1,19 @@
+package org.iana.rzm.facade.auth;
+
+import org.iana.rzm.facade.user.UserVO;
+
+/**
+* @author Piotr Tkaczyk
+*/
+
+public class TestAuthenticatedUser {
+    AuthenticatedUser authUser;
+
+    public TestAuthenticatedUser(UserVO user) {
+        authUser = new AuthenticatedUser(user);
+    }
+
+    public AuthenticatedUser getAuthUser() {
+        return authUser;
+    }
+}
