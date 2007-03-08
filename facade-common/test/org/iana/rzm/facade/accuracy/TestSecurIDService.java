cf580d97d8034603ad4b2d8c16f4b3fa0a446576
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-08 17:04:32 +0000

    Refactored Authenticator beans.

---
+++ facade-common/test/org/iana/rzm/facade/accuracy/TestSecurIDService.java
@@ -0,0 +1,14 @@
+package org.iana.rzm.facade.accuracy;
+
+import org.iana.securid.SecurIDService;
+import org.iana.securid.InvalidAuthenticationDataException;
+
+/**
+ * @author Patrycja Wegrzynowicz
+ */
+public class TestSecurIDService implements SecurIDService {
+
+    public void authenticate(String userName, String securID) throws InvalidAuthenticationDataException { 
+
+    }
+}
