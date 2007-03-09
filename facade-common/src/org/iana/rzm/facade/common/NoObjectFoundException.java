adcec4f4b3a7e2269239fe5eba911be61ab001ab
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-09 11:15:00 +0000

    added: new field name

--- facade-common/src/org/iana/rzm/facade/common/NoObjectFoundException.java
+++ facade-common/src/org/iana/rzm/facade/common/NoObjectFoundException.java
@@ -6,12 +6,21 @@ package org.iana.rzm.facade.common;
 public class NoObjectFoundException extends Exception {
 
     private long id;
+    private String name;
 
     public NoObjectFoundException(long id) {
         this.id = id;
     }
 
+    public NoObjectFoundException(String name) {
+        this.name = name;
+    }
+
     public long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
     }
 }
