15ada6a1db0ca8eb8a2b6a98ffb5f479e5bb7b9f
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-12 15:01:33 +0000

    new method to find all process instances for given domain added to transaction manager

--- trunk/eiana-trans/src/org/iana/rzm/trans/TransactionManager.java
+++ trunk/eiana-trans/src/org/iana/rzm/trans/TransactionManager.java
@@ -1,6 +1,7 @@
 package org.iana.rzm.trans;
 
 import org.iana.rzm.domain.Domain;
+import org.jbpm.graph.exe.ProcessInstance;
 
 import java.util.List;
 
@@ -30,4 +31,6 @@ public interface TransactionManager {
     List<Transaction> findAll();
 
     List<Transaction> find(TransactionCriteria criteria);
+
+    public List<ProcessInstance> findAllProcessInstances(String domainName); 
 }
