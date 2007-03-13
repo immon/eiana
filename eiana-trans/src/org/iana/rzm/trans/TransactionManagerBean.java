dedb0d745d86218f04124a7b5ccd7d9b216cb6c7
lukaszz <lukaszz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-13 10:58:47 +0000

    Failed commit: Default (2)

--- eiana-trans/src/org/iana/rzm/trans/TransactionManagerBean.java
+++ eiana-trans/src/org/iana/rzm/trans/TransactionManagerBean.java
@@ -19,12 +19,13 @@ import java.util.ArrayList;
  */
 public class TransactionManagerBean implements TransactionManager {
     private JbpmContext context;
-    private TransactionDAO dao;
+    private TransactionDAO transactionDAO;
     private TicketingService ticketingService;
+    private DomainDAO domainDAO;
 
     public TransactionManagerBean(JbpmContext context, TransactionDAO dao, TicketingService ticketingService) {
         this.context = context;
-        this.dao = dao;
+        this.transactionDAO = dao;
         this.ticketingService = ticketingService;
     }
 
@@ -37,6 +38,10 @@ public class TransactionManagerBean implements TransactionManager {
     }
 
     public Transaction create(Domain domain) {
+        return null;
+    }
+
+    public Transaction modify(Domain domain) {
         TransactionData td = new TransactionData();
         td.setActions(createActions(domain));
         ProcessInstance pi = new ProcessInstance(context.getGraphSession().findLatestProcessDefinition("process trans test"));
@@ -45,10 +50,6 @@ public class TransactionManagerBean implements TransactionManager {
         pi.getContextInstance().setVariable("TRANSACTION_DATA", td);
         pi.getContextInstance().setVariable("TRACK_DATA", new TrackData());
         return new Transaction(pi);
-    }
-
-    public Transaction modify(Domain domain) {
-        return null;
     }
 
     public List<Transaction> findAll() {
@@ -60,7 +61,7 @@ public class TransactionManagerBean implements TransactionManager {
     }
 
     public List<Transaction> findAllProcessInstances(String domainName) {
-        List<ProcessInstance> processInstances = dao.findAllProcessInstances(domainName);
+        List<ProcessInstance> processInstances = transactionDAO.findAllProcessInstances(domainName);
         List<Transaction> result = new ArrayList<Transaction>();
         for (ProcessInstance pi : processInstances) result.add(new Transaction(pi));
         return result;
@@ -69,8 +70,7 @@ public class TransactionManagerBean implements TransactionManager {
     private List<TransactionAction> createActions(Domain domain) {
         List<TransactionAction> resultList = new ArrayList<TransactionAction>();
         CheckTool.checkNull(domain, "new domain");
-        DomainDAO dao = (DomainDAO) new ClassPathXmlApplicationContext("eiana-domains-spring.xml").getBean("domainDAO");
-        Domain originDomain = dao.get(domain.getName());
+        Domain originDomain = domainDAO.get(domain.getName());
         CheckTool.checkNull(originDomain, "origin domain");
 
         TransactionActionsListBuilder.addSupportingOrganizationAction(domain, originDomain, resultList);
