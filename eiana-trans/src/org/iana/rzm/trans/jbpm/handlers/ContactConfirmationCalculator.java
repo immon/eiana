1ef1652dc2e67a9eaf6d96401cde8b1bdf71d47f
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-21 12:04:06 +0000

    JBPM handlers.

---
+++ eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/ContactConfirmationCalculator.java
@@ -0,0 +1,17 @@
+package org.iana.rzm.trans.jbpm.handlers;
+
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.exe.ExecutionContext;
+
+/**
+ * This class calculates parties that need to consent upon a process proceeds to the next state
+ * from the PENDING_CONTACT_CONFIRMATION state.
+ *
+ * @author Patrycja Wegrzynowicz
+ */
+public class ContactConfirmationCalculator implements ActionHandler {
+
+    public void execute(ExecutionContext executionContext) throws Exception {
+
+    }
+}
---
+++ eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/RoleCalculator.java
@@ -0,0 +1,16 @@
+package org.iana.rzm.trans.jbpm.handlers;
+
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.exe.ExecutionContext;
+
+/**
+ * @author Patrycja Wegrzynowicz
+ */
+public class RoleCalculator implements ActionHandler {
+
+    String role;
+
+    public void execute(ExecutionContext executionContext) throws Exception {
+
+    }
+}
