a0039f3e65e97f2448445624fcff319c8e45e392
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-21 15:23:46 +0000

    added  tests for UpdateDomainAction

---
+++ trunk/eiana-trans/test/jbpm/org/iana/rzm/trans/JbpmUpdateDomainTest.java
@@ -0,0 +1,32 @@
+/**
+ * @author Piotr Tkaczyk
+ */
+package org.iana.rzm.trans;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.iana.rzm.domain.dao.DomainDAO;
+import org.iana.rzm.domain.Domain;
+
+import java.net.URL;
+
+@Test(sequential=true, groups = {"jbpm", "accuracy", "eiana-trans"})
+public class JbpmUpdateDomainTest {
+
+    ApplicationContext ctx;
+    JbpmUpdateDomain updateDomainTest;
+
+    @BeforeTest
+    public void init() {
+        ctx = new ClassPathXmlApplicationContext("eiana-trans-spring.xml");
+        updateDomainTest = (JbpmUpdateDomain) ctx.getBean("UpdateDomainTest");
+        updateDomainTest.setContext(ctx);
+    }
+
+    @Test
+    public void test() throws Exception {
+        assert updateDomainTest.doUpdate();
+    }
+}
