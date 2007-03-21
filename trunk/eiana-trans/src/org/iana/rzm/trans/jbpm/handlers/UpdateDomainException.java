bdbc86f3e584259730b8aade75e361484017e078
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-21 15:20:34 +0000

    added  UpdateDomainAction handler and exception

---
+++ trunk/eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/UpdateDomainException.java
@@ -0,0 +1,11 @@
+/**
+ * @author Piotr Tkaczyk
+ */
+package org.iana.rzm.trans.jbpm.handlers;
+
+public class UpdateDomainException extends Exception {
+
+    public UpdateDomainException (String message) {
+        super(message);
+    }
+}
