ecc5d3010f7832bcb0ab8a8b19397cce7b2093fd
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-23 17:15:50 +0000

    transaction management added

--- eiana-users/test/dao/org/iana/rzm/user/dao/accuracy/UserManagerBeanAccuracyTest.java
+++ eiana-users/test/dao/org/iana/rzm/user/dao/accuracy/UserManagerBeanAccuracyTest.java
@@ -1,16 +1,20 @@
 package org.iana.rzm.user.dao.accuracy;
 
 import org.iana.rzm.user.*;
-import org.iana.rzm.user.dao.common.UserManagementTestUtil;
 import org.iana.rzm.user.dao.UserDAO;
+import org.iana.rzm.user.dao.common.UserManagementTestUtil;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.HashSet;
 
 /**
  * @author Piotr Tkaczyk
@@ -20,16 +24,20 @@ public class UserManagerBeanAccuracyTest {
     UserManager manager;
     Long        userId;
     UserDAO dao;
+    private PlatformTransactionManager txMgr;
+    private TransactionDefinition txDef = new DefaultTransactionDefinition();
 
     @BeforeClass
     public void init() {
-        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("eiana-users-spring.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("eiana-users-spring.xml");
         manager = (UserManager) ctx.getBean("userManager");
         dao = (UserDAO) ctx.getBean("userDAO");
+        txMgr = (PlatformTransactionManager) ctx.getBean("transactionManager");
     }
 
     @Test
     public void testCreateUser() {
+        TransactionStatus txStatus = txMgr.getTransaction(txDef);
         RZMUser userCreated = new RZMUser();
         userCreated.setFirstName("Ivan");
         userCreated.setLastName("Delphin");
@@ -41,34 +49,39 @@ public class UserManagerBeanAccuracyTest {
 
         assert userRetrieved.getFirstName().equals("Ivan");
         assert userRetrieved.getLastName().equals("Delphin");
+        txMgr.commit(txStatus);
     }
 
     @Test(dependsOnMethods = {"testCreateUser"})
     public void testGetUserById() throws Exception {
+        TransactionStatus txStatus = txMgr.getTransaction(txDef);
         RZMUser userRetrived = manager.get(userId);
         assert userRetrived != null;
         assert userRetrived.getRoles().size() == 1;
         Role role = userRetrived.getRoles().iterator().next();
         assert role instanceof AdminRole;
         AdminRole ar = (AdminRole) role;
-        assert AdminRole.AdminType.GOV_OVERSIGHT.equals(ar.getTrackData());
+        assert AdminRole.AdminType.GOV_OVERSIGHT.equals(ar.getType());
         assert userRetrived.getFirstName().equals("Ivan");
         assert userRetrived.getLastName().equals("Delphin");
         assert userRetrived.getLoginName().equals("ivan123");
+        txMgr.commit(txStatus);
     }
 
     @Test(dependsOnMethods = {"testGetUserById"})
     public void testGetUserByName() throws Exception {
+        TransactionStatus txStatus = txMgr.getTransaction(txDef);
         RZMUser userRetrived = manager.get("ivan123");
         assert userRetrived != null;
         assert userRetrived.getRoles().size() == 1;
         Role role = userRetrived.getRoles().iterator().next();
         assert role instanceof AdminRole;
         AdminRole ar = (AdminRole) role;
-        assert AdminRole.AdminType.GOV_OVERSIGHT.equals(ar.getTrackData());
+        assert AdminRole.AdminType.GOV_OVERSIGHT.equals(ar.getType());
         assert userRetrived.getFirstName().equals("Ivan");
         assert userRetrived.getLastName().equals("Delphin");
         assert userRetrived.getLoginName().equals("ivan123");
+        txMgr.commit(txStatus);
     }
 
     @Test
