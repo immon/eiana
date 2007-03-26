2263100d54866135a1cedc368b8433a6abb658bd
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-26 11:48:16 +0000

    new method getGraphSession

--- eiana-trans/src/org/iana/rzm/trans/dao/ProcessDAO.java
+++ eiana-trans/src/org/iana/rzm/trans/dao/ProcessDAO.java
@@ -2,6 +2,8 @@ package org.iana.rzm.trans.dao;
 
 import org.jbpm.graph.def.ProcessDefinition;
 import org.jbpm.graph.exe.ProcessInstance;
+import org.jbpm.db.GraphSession;
+import org.jbpm.JbpmContext;
 
 import java.util.List;
 
@@ -9,6 +11,7 @@ import java.util.List;
  * @author Jakub Laszkiewicz
  */
 public interface ProcessDAO {
+    public JbpmContext getContext();
     public ProcessInstance getProcessInstance(final long processInstanceId);
     public ProcessInstance newProcessInstance(final String name);
     public ProcessInstance newProcessInstanceForUpdate(final String name);
@@ -16,4 +19,5 @@ public interface ProcessDAO {
     public void deploy(final ProcessDefinition pd);
     public void save(ProcessInstance pi);
     public void close();
+    public GraphSession getGraphSession();
 }
