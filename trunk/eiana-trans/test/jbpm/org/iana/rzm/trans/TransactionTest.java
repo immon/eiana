2ac5afa32cb55fb634f8b26d9243467c349b5b87
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-27 10:14:26 +0000

    temp version

--- trunk/eiana-trans/test/jbpm/org/iana/rzm/trans/TransactionTest.java
+++ trunk/eiana-trans/test/jbpm/org/iana/rzm/trans/TransactionTest.java
@@ -2,12 +2,20 @@ package org.iana.rzm.trans;
 
 import org.iana.rzm.common.TrackData;
 import org.iana.rzm.trans.dao.ProcessDAO;
+import org.iana.rzm.trans.conf.SpringApplicationContext;
+import org.iana.rzm.trans.conf.DefinedTestProcess;
 import org.jbpm.JbpmContext;
 import org.jbpm.graph.def.ProcessDefinition;
 import org.jbpm.graph.exe.ProcessInstance;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
 
 /**
  * @author Jakub Laszkiewicz
@@ -20,21 +28,27 @@ public class TransactionTest {
 
     private long ticketId;
 
-    private ProcessDAO dao;
+    private ProcessDAO procesDAO;
 
     private TransactionManager manager;
+
+    private PlatformTransactionManager txMgr;
+    private TransactionDefinition txDef = new DefaultTransactionDefinition();
+    TransactionStatus txStatus;
+
     @BeforeClass(groups = {"accuracy", "eiana-trans", "jbpm","transaction"})
     public void init() {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("eiana-trans-spring.xml");
-        dao = (ProcessDAO) ctx.getBean("processDAO");
+        ApplicationContext ctx = SpringApplicationContext.getInstance().getContext();
+        procesDAO = (ProcessDAO) ctx.getBean("processDAO");
         manager = (TransactionManager) ctx.getBean("transactionManagerBean");
-        deployProcessDefinition();
+        procesDAO.deploy(deployProcessDefinition());
+        txMgr = (PlatformTransactionManager) ctx.getBean("transactionManager");
+        txStatus = txMgr.getTransaction(txDef);
     }
 
     @Test(dependsOnGroups = "simple",groups = {"accuracy", "eiana-trans", "jbpm","transaction"})
     public void testTransactionCreation() throws TransactionException {
-        JbpmContext context = JbpmTestContextFactory.getJbpmContext();
-        ProcessInstance pi = new ProcessInstance(context.getGraphSession().findLatestProcessDefinition("process trans test"));        
+        ProcessInstance pi = procesDAO.newProcessInstance("Domain Modification Transaction (Unified Workflow)");
         TransactionData td = new TransactionData();
         ticketId = 123L;
         td.setTicketID(ticketId);
@@ -42,45 +56,47 @@ public class TransactionTest {
         pi.getContextInstance().setVariable("TRACK_DATA",new TrackData());
         pi.signal();
         Transaction transaction = new Transaction(pi);
-        context.close();
+        //context.close();
         transactionId = transaction.getTransactionID();
-        context = JbpmTestContextFactory.getJbpmContext();
-        manager.setJBPMContext(context);
+        //context = procesDAO.getContext();
+        //manager.setJBPMContext(context);
         Transaction transFromDB = manager.getTransaction(transactionId);
         assert (transFromDB != null && transFromDB.getTransactionID() == transactionId && transFromDB.getTicketID().equals(new Long(ticketId)));
     }
 
     @Test(dependsOnMethods = ("testTransactionCreation"),groups = {"accuracy", "eiana-trans", "jbpm","transaction"})
-    public void testTranactionUpdate() throws NoSuchTransactionException {
-        JbpmContext context = JbpmTestContextFactory.getJbpmContext();
-        manager.setJBPMContext(context);
+    public void testTransactionUpdate() throws NoSuchTransactionException {
+        //JbpmContext context = procesDAO.getContext();
+        //manager.setJBPMContext(context);
         Transaction transToUpdate = manager.getTransaction(transactionId);
         assert transToUpdate.getTicketID().equals(new Long(ticketId));
         ticketId = 456L;
         transToUpdate.setTicketID(ticketId);
-        context.close();
-        context = JbpmTestContextFactory.getJbpmContext();
-        manager.setJBPMContext(context);
+        //context.close();
+        //context = procesDAO.getContext();
+        //manager.setJBPMContext(context);
         Transaction transFromDB = manager.getTransaction(transactionId);
         assert (transFromDB != null && transFromDB.getTransactionID() == transactionId && transFromDB.getTicketID().equals(new Long(ticketId)));
-        context.close();
+        //context.close();
+       // procesDAO.close();
     }
 
-    @Test(dependsOnMethods = ("testTranactionUpdate"),groups = {"accuracy", "eiana-trans", "jbpm","transaction"})
-       public void testTranactionAccept() throws TransactionException {
-           JbpmContext context = JbpmTestContextFactory.getJbpmContext();
-           manager.setJBPMContext(context);
+    @Test(dependsOnMethods = ("testTransactionUpdate"),groups = {"accuracy", "eiana-trans", "jbpm","transaction"})
+       public void testTransactionAccept() throws TransactionException {
+           //JbpmContext context = procesDAO.getContext();
+           //manager.setJBPMContext(context);
            Transaction trans = manager.getTransaction(transactionId);
+            
            assert trans != null;
            trans.accept(null);
            System.out.println("State:"+trans.getState().getName());
            assert trans.getState().getName().equals(TransactionState.Name.COMPLETED);           
        }
 
-     @Test(dependsOnMethods = ("testTranactionUpdate"),groups = {"accuracy", "eiana-trans", "jbpm","transaction"})
-       public void testTranactionReject() throws NoSuchTransactionException {
-           JbpmContext context = JbpmTestContextFactory.getJbpmContext();
-           manager.setJBPMContext(context);
+     @Test(dependsOnMethods = ("testTransactionUpdate"),groups = {"accuracy", "eiana-trans", "jbpm","transaction"})
+       public void testTransactionReject() throws NoSuchTransactionException {
+           //JbpmContext context = procesDAO.getContext();
+           //manager.setJBPMContext(context);
            Transaction trans = manager.getTransaction(transactionId);
            assert trans != null;
            trans.reject();
@@ -88,13 +104,13 @@ public class TransactionTest {
            assert trans.getState().getName().equals(TransactionState.Name.REJECTED);           
        }
 
-    
-       private void deployProcessDefinition() {
-        ProcessDefinition processDefinition = ProcessDefinition.parseXmlString(
-                "<process-definition name='process trans test'>" +
-                        "  <start-state name='PENDING_IANA_CONFIRMATION'>" +
-                        "    <transition to='first' />" +
-                        "  </start-state>" +
+
+       private ProcessDefinition deployProcessDefinition() {
+        return ProcessDefinition.parseXmlString(
+                "<process-definition name='Domain Modification Transaction (Unified Workflow)'>\n" +
+                        "    <start-state>\n" +
+                        "        <transition to='first' />\n" +
+                        "    </start-state>\n" +
                         "   <task-node name='first'>" +
                             "    <task name='doSmth'></task>" +
                             "    <transition name='ok' to='COMPLETED' />" +
@@ -104,12 +120,12 @@ public class TransactionTest {
                         "  <end-state name='REJECTED' />" +
                         "</process-definition>"
         );
-
-        JbpmContext jbpmContext = JbpmTestContextFactory.getJbpmContext();
-        try {
-            jbpmContext.deployProcessDefinition(processDefinition);
-        } finally {
-            jbpmContext.close();
-        }
     }
+
+    @AfterClass
+    public void finish() {
+
+       // procesDAO.close();
+    }
+
 }
