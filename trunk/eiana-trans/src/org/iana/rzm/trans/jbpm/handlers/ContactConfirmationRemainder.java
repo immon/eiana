c6e0d84775e9ad142d74bc8697574f4a4ec29b37
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-03-30 11:54:08 +0000

    test implementation

---
+++ trunk/eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/ContactConfirmationRemainder.java
@@ -0,0 +1,55 @@
+/**
+ * @author Piotr Tkaczyk
+ */
+package org.iana.rzm.trans.jbpm.handlers;
+
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.exe.ExecutionContext;
+import org.jbpm.configuration.ObjectFactory;
+import org.iana.rzm.user.RZMUser;
+import org.iana.rzm.user.Role;
+import org.iana.rzm.user.SystemRole;
+import org.iana.rzm.user.UserManager;
+import org.iana.notifications.template.TemplateContactConfirmation;
+import org.iana.notifications.template.TemplateContactConfirmationRemainder;
+import org.iana.notifications.Notification;
+import org.iana.notifications.EmailAddress;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class ContactConfirmationRemainder extends ProcessStateNotifier implements ActionHandler {
+
+    int period;
+    
+    public void execute(ExecutionContext executionContext) throws Exception {
+        fillDataFromContext(executionContext);
+
+        Set<RZMUser> users = getAllUsersInRole(executionContext, td.getCurrentDomain().getName());
+
+        for(RZMUser user : users)
+            sendContactNotification(td.getCurrentDomain().getName(), user);
+    }
+
+    private void sendContactNotification(String domainName, RZMUser user) throws Exception {
+        for (Role role : user.getRoles()) {
+            if (((SystemRole)role).isNotify()) {
+                TemplateContactConfirmationRemainder tCCR = new TemplateContactConfirmationRemainder(domainName, ((SystemRole)role).getFullTypeName(), user.mustAccept(domainName, role.getType()), 30-period);
+                Notification notification = notificationTemplate.getNotificationInstance(tCCR);
+                String userFullName = user.getFirstName() + " " + user.getLastName();
+                sendNotification(new EmailAddress(userFullName, user.getEmail()), notification);
+            }
+        }
+    }
+
+    private Set<RZMUser> getAllUsersInRole(ExecutionContext executionContext, String domainName) {
+        ObjectFactory of = executionContext.getJbpmContext().getObjectFactory();
+        UserManager um = (UserManager) of.createObject("userManager");
+        Set<RZMUser> users = new HashSet<RZMUser>();
+        users.addAll(um.findUsersRequiredToConfirm(domainName, SystemRole.SystemType.AC));
+        users.addAll(um.findUsersRequiredToConfirm(domainName, SystemRole.SystemType.TC));
+        users.addAll(um.findUsersEligibleToConfirm(domainName, SystemRole.SystemType.AC));
+        users.addAll(um.findUsersEligibleToConfirm(domainName, SystemRole.SystemType.TC));
+        return users;
+    }
+}
