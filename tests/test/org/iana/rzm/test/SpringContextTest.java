a0be856c6398ecf92092cc38b24f7107f4b18a0a
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-10 18:38:19 +0000

    Facade services configured in spring services file.

---
+++ tests/test/org/iana/rzm/test/SpringContextTest.java
@@ -0,0 +1,25 @@
+package org.iana.rzm.test;
+
+import org.testng.annotations.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import java.io.File;
+
+/**
+ * Tests a syntax of spring services configuration.
+ *
+ * @author Patrycja Wegrzynowicz
+ */
+@Test(groups = {"spring-test"})
+public class SpringContextTest {
+
+    @Test
+    public void testSpringServiceConfig() {
+
+        System.out.println(new File("./conf/spring/services-config.xml").getAbsolutePath());
+        ApplicationContext appContext = new FileSystemXmlApplicationContext("./conf/spring/services-config.xml");
+        assert appContext != null;
+    }
+}
