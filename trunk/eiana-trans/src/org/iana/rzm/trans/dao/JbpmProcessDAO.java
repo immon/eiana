3ee15a224678415e2e76026c49c532e6990bafde
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-11 13:16:55 +0000

    new methods to find processes/transactions by user and/or domain name added to ProcessDAO and TransactionManager, respectively

--- trunk/eiana-trans/src/org/iana/rzm/trans/dao/JbpmProcessDAO.java
+++ trunk/eiana-trans/src/org/iana/rzm/trans/dao/JbpmProcessDAO.java
@@ -2,8 +2,8 @@ package org.iana.rzm.trans.dao;
 
 import org.hibernate.Query;
 import org.iana.rzm.trans.jbpm.JbpmContextFactory;
+import org.iana.rzm.user.RZMUser;
 import org.jbpm.JbpmContext;
-import org.jbpm.db.GraphSession;
 import org.jbpm.graph.def.ProcessDefinition;
 import org.jbpm.graph.exe.ProcessInstance;
 
@@ -50,6 +50,50 @@ public class JbpmProcessDAO implements ProcessDAO {
                         "   and pi = hli.processInstance " +
                         "   and domain.name.name = :name"
         );
+        query.setString("name", domainName);
+        return query.list();
+    }
+
+    public List<ProcessInstance> findAllProcessInstances(final RZMUser user) {
+        if (user == null) throw new IllegalArgumentException("user is null");
+        Query query = getContext().getSession().createQuery(
+                "select pi " +
+                        "from ProcessInstance as pi, " +
+                        "HibernateLongInstance as hli, " +
+                        "TransactionData as td " +
+                        "   inner join td.currentDomain as domain, " +
+                        "RZMUser as user " +
+                        "   inner join user.roles as role " +
+                        "where hli.value.class = 'org.iana.rzm.trans.TransactionData' " +
+                        "   and hli.value.id = td.objId " +
+                        "   and pi = hli.processInstance " +
+                        "   and user.objId = :id " +
+                        "   and role.class = SystemRole " +
+                        "   and domain.name.name = role.name"
+        );
+        query.setLong("id", user.getObjId());
+        return query.list();
+    }
+
+    public List<ProcessInstance> findAllProcessInstances(final RZMUser user, final String domainName) {
+        if (user == null) throw new IllegalArgumentException("user is null");
+        Query query = getContext().getSession().createQuery(
+                "select pi " +
+                        "from ProcessInstance as pi, " +
+                        "HibernateLongInstance as hli, " +
+                        "TransactionData as td " +
+                        "   inner join td.currentDomain as domain, " +
+                        "RZMUser as user " +
+                        "   inner join user.roles as role " +
+                        "where hli.value.class = 'org.iana.rzm.trans.TransactionData' " +
+                        "   and hli.value.id = td.objId " +
+                        "   and pi = hli.processInstance " +
+                        "   and user.objId = :id " +
+                        "   and role.class = SystemRole " +
+                        "   and domain.name.name = :name " +
+                        "   and role.name = :name"
+        );
+        query.setLong("id", user.getObjId());
         query.setString("name", domainName);
         return query.list();
     }
