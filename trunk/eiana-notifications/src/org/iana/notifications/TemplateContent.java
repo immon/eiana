c5603153a5025e49cc297596157beb0ed4e9870b
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-12 16:10:38 +0000

    TemplateContent now extends Content

--- trunk/eiana-notifications/src/org/iana/notifications/TemplateContent.java
+++ trunk/eiana-notifications/src/org/iana/notifications/TemplateContent.java
@@ -1,16 +1,28 @@
 package org.iana.notifications;
 
+import org.hibernate.annotations.CollectionOfElements;
+
+import javax.persistence.*;
 import java.util.Map;
 
 /**
  * This class represents content based on a text template instantiated with a set of string parameters.
  *
  * @author Patrycja Wegrzynowicz
+ * @author Piotr    Tkaczyk
  */
-public class TemplateContent implements Content {
 
+@Entity
+public class TemplateContent extends Content {
+
+    @Basic
     private String templateName;
+
+    @CollectionOfElements
+    @JoinTable(name="TemplateContent_values")
     private Map<String, String> values;
+
+    protected TemplateContent() {}
 
     public TemplateContent(String templateName) {
         this.templateName = templateName;
@@ -44,5 +56,17 @@ public class TemplateContent implements Content {
 
     public String getBody() {
         return null;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TemplateContent tc = (TemplateContent) o;
+
+        if (!templateName.equals(tc.getTemplateName())) return false;
+        if (!values.equals(tc.getValues())) return false;
+
+        return true;
     }
 }
