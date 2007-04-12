79c2bc2b2c8960298fc88ce32d52eb7c7b2dd8bd
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-12 16:05:37 +0000

    notifications dao and manager added

---
+++ eiana-notifications/src/org/iana/notifications/dao/NotificationDAO.java
@@ -0,0 +1,18 @@
+package org.iana.notifications.dao;
+
+import org.iana.notifications.Notification;
+import org.iana.notifications.Addressee;
+
+import java.util.List;
+
+/**
+ * @author Piotr Tkaczyk
+ */
+
+public interface NotificationDAO {
+    public Notification       get(long id);
+    public void               create(Notification notification);
+    public void               update(Notification notification);
+    public void               delete(Notification notification);
+    public List<Notification> findUserNotifications(Addressee addressee);
+}
