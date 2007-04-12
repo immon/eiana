1524e202ae2afe152a433008367e3d9c69f0f07c
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-12 16:04:59 +0000

    content is now abstract

--- trunk/eiana-notifications/src/org/iana/notifications/Content.java
+++ trunk/eiana-notifications/src/org/iana/notifications/Content.java
@@ -1,17 +1,29 @@
 package org.iana.notifications;
 
+import javax.persistence.*;
+
 /**
  * The markup interface representing a notification content.
  * The provided classes (@see TextContent, @see TemplateContent) may be used
  * as well as a custom implemention may be provided.
  *
  * @author Patrycja Wegrzynowicz
+ * @author Piotr    Tkaczyk
  * @see TextContent
  * @see TemplateContent
  */
-public interface Content {
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+public abstract class Content {
 
-    String getSubject();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long objId;
 
-    String getBody();
+    public Long getObjId() {
+        return objId;
+    }
+
+    public abstract String getSubject();
+    public abstract String getBody();
 }
