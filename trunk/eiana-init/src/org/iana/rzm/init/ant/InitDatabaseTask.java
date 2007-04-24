03ec3c1a4a3c912f85fda11c874041a85ad87d11
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-24 13:52:39 +0000

    single database initialization ant task created; system users initialization added

--- trunk/eiana-init/src/org/iana/rzm/init/ant/InitDatabaseTask.java
+++ trunk/eiana-init/src/org/iana/rzm/init/ant/InitDatabaseTask.java
@@ -1,17 +1,20 @@
 package org.iana.rzm.init.ant;
 
 import org.hibernate.Session;
-import org.iana.rzm.domain.*;
-import org.iana.rzm.common.exceptions.InvalidNameException;
 import org.iana.rzm.common.exceptions.InvalidIPAddressException;
-import org.apache.tools.ant.BuildException;
+import org.iana.rzm.common.exceptions.InvalidNameException;
+import org.iana.rzm.domain.*;
+import org.iana.rzm.user.AdminRole;
+import org.iana.rzm.user.MD5Password;
+import org.iana.rzm.user.RZMUser;
+import org.iana.rzm.user.SystemRole;
 
 import java.net.MalformedURLException;
 
 /**
  * @author Jakub Laszkiewicz
  */
-public class InitDomainsTask extends HibernateTask {
+public class InitDatabaseTask extends HibernateTask {
     private Address setupAddress(Address address, String prefix, String countryCode) {
         address.setTextAddress(prefix + " text address");
         address.setCountryCode(countryCode);
@@ -50,18 +53,84 @@ public class InitDomainsTask extends HibernateTask {
         domain.addTechContact(setupContact(new Contact(), "tech", name, "US"));
         return domain;
     }
-    
+
+    private RZMUser setupUser(RZMUser user, String name) {
+        user.setEmail(name + "-@no-mail.org");
+        user.setFirstName(name);
+        user.setLastName(name);
+        user.setLoginName(name);
+        user.setOrganization(name);
+        user.setPassword(new MD5Password(name));
+        user.setSecurID(false);
+        return user;
+    }
+
+    private SystemRole setupSystemRole(SystemRole role, String name, boolean acceptFrom,
+                                       boolean mustAccept, boolean notify) throws InvalidNameException {
+        role.setName(name);
+        role.setAcceptFrom(acceptFrom);
+        role.setMustAccept(mustAccept);
+        role.setNotify(notify);
+        return role;
+    }
+
+    private RZMUser setupSystemUser(String name, SystemRole role) {
+        RZMUser user = new RZMUser();
+        user.setEmail(name + "@no-mail.org");
+        user.setFirstName(name + " first name");
+        user.setLastName(name + " last name");
+        user.setLoginName(name);
+        user.setOrganization(name + " organization");
+        user.setPassword(new MD5Password(name + "password"));
+        user.setSecurID(false);
+        user.addRole(role);
+        return user;
+    }
+
     private static String[] domains = {"aero", "biz", "com", "coop", "info", "jobs", "museum", "name", "net", "org", "pro", "travel", "gov", "edu", "mil", "int", "ac", "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "at", "au", "aw", "az", "ax", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "sv", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "yu", "za", "zm", "zw"};
 
+
     public void doExecute(Session session) throws MalformedURLException, NameServerAlreadyExistsException, InvalidIPAddressException {
-        for (int i = 1; i < domains.length; i++) {
-            Domain domain = setupDomain(domains[i]);
-            session.save(domain);
+        RZMUser iana = setupUser(new RZMUser(), "iana");
+        iana.addRole(new AdminRole(AdminRole.AdminType.IANA));
+        session.save(iana);
+
+        RZMUser govOversight = setupUser(new RZMUser(), "gov_oversight");
+        govOversight.addRole(new AdminRole(AdminRole.AdminType.GOV_OVERSIGHT));
+        session.save(govOversight);
+
+        RZMUser zonePublisher = setupUser(new RZMUser(), "zone_publisher");
+        zonePublisher.addRole(new AdminRole(AdminRole.AdminType.ZONE_PUBLISHER));
+        session.save(zonePublisher);
+
+        for (String domain: domains) {
+            session.save(setupSystemUser(domain + "-ac1", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.AC), domain, true, true, true)));
+            session.save(setupSystemUser(domain + "-ac2", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.AC), domain, true, false, true)));
+            session.save(setupSystemUser(domain + "-ac3", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.AC), domain, false, false, true)));
+
+            session.save(setupSystemUser(domain + "-so1", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.SO), domain, true, true, true)));
+            session.save(setupSystemUser(domain + "-so2", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.SO), domain, true, false, true)));
+            session.save(setupSystemUser(domain + "-so3", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.SO), domain, false, false, true)));
+
+            session.save(setupSystemUser(domain + "-tc1", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.TC), domain, true, true, true)));
+            session.save(setupSystemUser(domain + "-tc2", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.TC), domain, true, false, true)));
+            session.save(setupSystemUser(domain + "-tc3", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.TC), domain, false, false, true)));
+
+            session.save(setupDomain(domain));
         }
     }
 
     public static void main(String[] args) {
-        InitDomainsTask task = new InitDomainsTask();
+        InitDatabaseTask task = new InitDatabaseTask();
         task.setAnnotationConfiguration("hibernate.cfg.xml");
         task.execute();
     }
