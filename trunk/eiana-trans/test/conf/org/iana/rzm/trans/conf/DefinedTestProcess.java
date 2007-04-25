aad81003eef8ca09057cdea5f75b6a50ead5468a
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-25 07:43:47 +0000

    path variable removed

--- trunk/eiana-trans/test/conf/org/iana/rzm/trans/conf/DefinedTestProcess.java
+++ trunk/eiana-trans/test/conf/org/iana/rzm/trans/conf/DefinedTestProcess.java
@@ -4,10 +4,6 @@
 package org.iana.rzm.trans.conf;
 
 import org.jbpm.graph.def.ProcessDefinition;
-import org.testng.annotations.Test;
-
-import java.io.FileReader;
-import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 
@@ -18,7 +14,6 @@ public class DefinedTestProcess {
     private static String processName;
 
     public static ProcessDefinition getDefinition()  {
-        String path[] = System.getProperty("java.class.path").split(";");
         InputStream inputStream = DefinedTestProcess.class.getClassLoader().getResourceAsStream(PROCESS_DEFINITION_FILE);
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         pd = ProcessDefinition.parseXmlReader(inputStreamReader);
