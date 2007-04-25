876b0be164ae364b6f09a3b52172b7c126166ac3
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-25 20:24:14 +0000

    Skeleton admin module.

--- trunk/facade-admin/src/org/iana/rzm/facade/admin/AdminDomainService.java
+++ trunk/facade-admin/src/org/iana/rzm/facade/admin/AdminDomainService.java
@@ -1,7 +1,29 @@
 package org.iana.rzm.facade.admin;
 
+import org.iana.rzm.facade.user.UserVO;
+import org.iana.rzm.facade.common.RZMStatefulService;
+import org.iana.rzm.facade.system.domain.DomainVO;
+
+import java.util.List;
+
 /**
  * @author Patrycja Wegrzynowicz
  */
-public class AdminDomainService {
+public interface AdminDomainService extends RZMStatefulService {
+
+    public DomainVO getDomain(String DomainName);
+
+    public DomainVO getDomain(long id);
+
+    public void createDomain(DomainVO Domain);
+
+    public void updateDomain(DomainVO Domain);
+
+    public void deleteDomain(String DomainName);
+
+    public void deleteDomain(long id);
+
+    public List<DomainVO> findDomains();
+
+    public List<DomainVO> findDomains(DomainCriteria criteria);
 }
--- trunk/facade-admin/src/org/iana/rzm/facade/admin/AdminUserService.java
+++ trunk/facade-admin/src/org/iana/rzm/facade/admin/AdminUserService.java
@@ -1,9 +1,29 @@
 package org.iana.rzm.facade.admin;
 
 import org.iana.rzm.facade.common.RZMStatefulService;
+import org.iana.rzm.facade.user.UserVO;
+
+import java.util.List;
 
 /**
  * @author Patrycja Wegrzynowicz
  */
 public interface AdminUserService extends RZMStatefulService {
+
+    public UserVO getUser(String userName);
+
+    public UserVO getUser(long id);
+
+    public void createUser(UserVO user);
+
+    public void updateUser(UserVO user);
+
+    public void deleteUser(String userName);
+
+    public void deleteUser(long id);
+
+    public List<UserVO> findUsers();
+
+    public List<UserVO> findUsers(UserCriteria criteria);
+   
 }
