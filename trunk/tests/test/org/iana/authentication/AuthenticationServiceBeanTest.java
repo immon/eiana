6ee540742fad065695bcbf7f60a93e63d177513a
marcinzzz <marcinzzz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-25 14:03:02 +0000

    feature: updated Spring configuration for AuthenticationServiceBean + tests

---
+++ trunk/tests/test/org/iana/authentication/AuthenticationServiceBeanTest.java
@@ -0,0 +1,281 @@
+package org.iana.authentication;
+
+import org.iana.rzm.facade.auth.*;
+import org.iana.rzm.facade.accuracy.TestUserManager;
+import org.iana.rzm.facade.accuracy.TestSecurIDService;
+import org.iana.rzm.conf.SpringApplicationContext;
+import org.iana.rzm.user.dao.UserDAO;
+import org.iana.rzm.user.RZMUser;
+import org.iana.rzm.user.AdminRole;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+/**
+ * org.iana.authentication.AuthenticationServiceBeanTest
+ * (C) Research and Academic Computer Network - NASK
+ * marcinz, 2007-04-25, 14:38:46
+ */
+public class AuthenticationServiceBeanTest {
+
+    private PlatformTransactionManager txMgr;
+    private UserDAO userDAO;
+    private TransactionDefinition txDef = new DefaultTransactionDefinition();
+    TransactionStatus txStatus;
+    private AuthenticationService authService;
+
+    private RZMUser testAdminUser;
+    private RZMUser testAdminUserWithSecurID;
+    private RZMUser testWrongPasswordUser;
+
+
+
+    @BeforeClass
+    public void init() {
+        ApplicationContext appCtx = SpringApplicationContext.getInstance().getContext();
+        txMgr = (PlatformTransactionManager) appCtx.getBean("transactionManager");
+        userDAO = (UserDAO) appCtx.getBean("userDAO");
+        authService = (AuthenticationService) appCtx.getBean("authenticationServiceBean");
+
+        createTestUsers();
+    }
+
+    private void createTestUsers() {
+        txStatus = txMgr.getTransaction(txDef);
+
+        testAdminUser = createTestAdminUser();
+        userDAO.create(testAdminUser);
+
+        testAdminUserWithSecurID = createTestAdminUserWithSecurID();
+        userDAO.create(testAdminUserWithSecurID);
+
+        testWrongPasswordUser = createTestWrongPasswordUser();
+        userDAO.create(testWrongPasswordUser);
+
+        txMgr.rollback(txStatus);
+    }
+
+    @Test
+    public void testAuthenticate() throws Exception {
+        txStatus = txMgr.getTransaction(txDef);
+
+        PasswordAuth passwordAuth = new PasswordAuth(TestUserManager.ADMIN_LOGIN_VALID, TestUserManager.ADMIN_PASSWORD_VALID);
+
+        AuthenticatedUser authenticatedUser = authService.authenticate(passwordAuth);
+        assert authenticatedUser != null;
+        assert TestUserManager.ADMIN_LOGIN_VALID.equals(authenticatedUser.getUserName());
+
+        txMgr.commit(txStatus);
+    }
+
+    //could be moved to failure package
+    @Test (expectedExceptions = {AuthenticationFailedException.class})
+    public void testAuthenticateNoUser() throws Exception {
+
+        PasswordAuth passwordAuth = new PasswordAuth();
+        passwordAuth.setUserName(TestUserManager.NON_EXIST_LOGIN);
+        passwordAuth.setPassword("foo");
+
+        authService.authenticate(passwordAuth);
+    }
+
+    //could be moved to failure package
+    @Test (expectedExceptions = {AuthenticationFailedException.class})
+    public void testAuthenticateWrongPassword() throws Exception {
+
+        PasswordAuth passwordAuth = new PasswordAuth(TestUserManager.WRONG_PASSWORD_LOGIN, TestUserManager.WRONG_PASSWORD_PASSWORD);
+
+        authService.authenticate(passwordAuth);
+    }
+
+    //from securID
+
+    @Test (expectedExceptions = {AuthenticationRequiredException.class})
+    public void testAuthenticateWrongSecurdIDNeeded() throws Exception {
+
+        PasswordAuth passwordAuth = new PasswordAuth(TestUserManager.ADMIN_WITH_SECURID_VALID_LOGIN, TestUserManager.ADMIN_WITH_SECURID_PASSWORD_VALID);
+
+        authService.authenticate(passwordAuth);
+    }
+
+    @Test
+    public void testAuthenticateWithSecurID() throws Exception {
+
+        PasswordAuth passwordAuth = new PasswordAuth(TestUserManager.ADMIN_WITH_SECURID_VALID_LOGIN, TestUserManager.ADMIN_WITH_SECURID_PASSWORD_VALID);
+
+        try {
+            authService.authenticate(passwordAuth);
+
+        } catch (AuthenticationRequiredException e) {
+            if (e.getRequired() == Authentication.SECURID) {
+
+                SecurIDAuth securIDAuth = new SecurIDAuth(
+                        TestSecurIDService.ADMIN_WITH_SECURID_SECURID_VALID_LOGIN,
+                        TestSecurIDService.ADMIN_WITH_SECURID_SECURID_VALID_PASSWORD);
+
+                AuthenticatedUser authenticatedUser = authService.authenticate(e.getToken(), securIDAuth);
+                assert TestUserManager.ADMIN_WITH_SECURID_VALID_LOGIN.equals(authenticatedUser.getUserName());
+                return;
+            }
+        }
+        assert false;
+    }
+
+    //It's more complicated scenerio (exception can be thrown in 2 places) and
+    //it's more reliable to catch exception in try..catch construction
+    @Test
+    public void testAuthenticateWrongSecurdID() throws Exception {
+
+        PasswordAuth passwordAuth = new PasswordAuth(TestUserManager.ADMIN_WITH_SECURID_VALID_LOGIN, TestUserManager.ADMIN_WITH_SECURID_PASSWORD_VALID);
+
+        try {
+            authService.authenticate(passwordAuth);
+
+        } catch (AuthenticationRequiredException e) {
+            if (e.getRequired() == Authentication.SECURID) {
+
+                SecurIDAuth securIDAuth = new SecurIDAuth(
+                        TestSecurIDService.ADMIN_WITH_SECURID_SECURID_VALID_LOGIN,
+                        TestSecurIDService.ADMIN_WITH_SECURID_SECURID_WRONG_PASSWORD);
+
+                try {
+                    authService.authenticate(e.getToken(), securIDAuth);
+
+                } catch (AuthenticationFailedException ee) {
+                    return;
+                }
+            }
+        }
+        assert false;
+    }
+
+    @Test
+    public void testAuthenticateOnlySecurID() throws Exception {
+
+        SecurIDAuth securIDAuth = new SecurIDAuth(
+                TestSecurIDService.ADMIN_WITH_SECURID_SECURID_VALID_LOGIN,
+                TestSecurIDService.ADMIN_WITH_SECURID_SECURID_VALID_PASSWORD);
+
+        try {
+            authService.authenticate(securIDAuth);
+
+        } catch (AuthenticationRequiredException e) {
+            if (e.getRequired() == Authentication.PASSWORD) {
+                return;
+            }
+        }
+        assert false;
+    }
+
+    //common
+
+    @Test (expectedExceptions = {IllegalArgumentException.class})
+    public void testAuthenticateWithNullData() throws Exception {
+
+        authService.authenticate(null);
+    }
+
+    @Test (expectedExceptions = {IllegalArgumentException.class})
+    public void testAuthenticateWithNullToken() throws Exception {
+
+        SecurIDAuth securIDAuth = new SecurIDAuth();
+        securIDAuth.setUserName(TestSecurIDService.ADMIN_WITH_SECURID_SECURID_VALID_LOGIN);
+        securIDAuth.setPassword(TestSecurIDService.ADMIN_WITH_SECURID_SECURID_VALID_PASSWORD);
+
+        authService.authenticate(null, securIDAuth);
+    }
+
+    @Test (expectedExceptions = {IllegalArgumentException.class})
+    public void testAuthenticateWithNullBoth() throws Exception {
+
+        authService.authenticate(null, null);
+    }
+
+    @Test
+    public void testInvalidateUser() throws AuthenticationFailedException, AuthenticationRequiredException {
+        PasswordAuth passwordAuth = new PasswordAuth(TestUserManager.ADMIN_LOGIN_VALID, TestUserManager.ADMIN_PASSWORD_VALID);
+
+        AuthenticatedUser authenticatedUser = authService.authenticate(passwordAuth);
+
+        assert authenticatedUser != null;
+        assert TestUserManager.ADMIN_LOGIN_VALID.equals(authenticatedUser.getUserName());
+        assert !authenticatedUser.isInvalidated();
+
+        authenticatedUser.invalidate();
+
+        assert authenticatedUser.isInvalidated();
+    }
+
+    @AfterClass
+    public void cleanUp() {
+
+        userDAO.delete(testAdminUser);
+        userDAO.delete(testAdminUserWithSecurID);
+        userDAO.delete(testWrongPasswordUser);
+    }
+
+
+    public static String NON_EXIST_LOGIN = "nonExistLogin";
+    public static String COMMON_FIRST_NAME = "commonFirstName";
+    public static String COMMON_LAST_NAME = "commonLastName";
+
+    public static String ADMIN_LOGIN_VALID = "adminLogin";
+    public static String ADMIN_PASSWORD_VALID = "adminPassword";
+    public static String ADMIN_FIRST_NAME_VALID = "adminFirstName";
+    public static String ADMIN_LAST_NAME_VALID = "adminLastName";
+
+    private RZMUser createTestAdminUser() {
+
+        RZMUser adminUser = new RZMUser();
+        adminUser.setObjId(1L);
+        adminUser.setFirstName(ADMIN_FIRST_NAME_VALID);
+        adminUser.setLastName(ADMIN_LAST_NAME_VALID);
+        adminUser.setLoginName(ADMIN_LOGIN_VALID);
+        adminUser.setPassword(ADMIN_PASSWORD_VALID);
+        adminUser.addRole(new AdminRole(AdminRole.AdminType.IANA));
+        adminUser.setSecurID(false);
+
+        return adminUser;
+    }
+
+    public static String ADMIN_WITH_SECURID_VALID_LOGIN = "adminWithSecurIDLogin";
+    public static String ADMIN_WITH_SECURID_PASSWORD_VALID = "adminWithSecurIDPassword";
+    public static String ADMIN_WITH_SECURID_FIRST_NAME_VALID = "adminWithSecurIDFirstName";
+    public static String ADMIN_WITH_SECURID_LAST_NAME_VALID = "adminWithSecurIDLastName";
+
+    private RZMUser createTestAdminUserWithSecurID() {
+
+        RZMUser adminUser = new RZMUser();
+        adminUser.setObjId(1L);
+        adminUser.setFirstName(ADMIN_WITH_SECURID_FIRST_NAME_VALID);
+        adminUser.setLastName(ADMIN_WITH_SECURID_LAST_NAME_VALID);
+        adminUser.setLoginName(ADMIN_WITH_SECURID_VALID_LOGIN);
+        adminUser.setPassword(ADMIN_WITH_SECURID_PASSWORD_VALID);
+        adminUser.addRole(new AdminRole(AdminRole.AdminType.IANA));
+        adminUser.setSecurID(true);
+
+        return adminUser;
+    }
+
+    public static String WRONG_PASSWORD_LOGIN = "wrongPasswordLogin";
+    public static String WRONG_PASSWORD_PASSWORD = "wrongPasswordLogin";
+
+    private RZMUser createTestWrongPasswordUser() {
+
+        RZMUser adminUser = new RZMUser();
+        adminUser.setObjId(1L);
+        adminUser.setFirstName(COMMON_FIRST_NAME);
+        adminUser.setLastName(COMMON_LAST_NAME);
+        adminUser.setLoginName(WRONG_PASSWORD_LOGIN);
+        adminUser.setPassword("bad" + WRONG_PASSWORD_PASSWORD);
+        adminUser.addRole(new AdminRole(AdminRole.AdminType.IANA));
+        adminUser.setSecurID(false);
+
+        return adminUser;
+    }
+}
