754456fbc2c317216318733e09c4e67dbe70b38c
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-27 09:41:24 +0000

    cleanUp method added

--- tests/test/org/iana/rzm/trans/SystemTransactionServiceBeanTest.java
+++ tests/test/org/iana/rzm/trans/SystemTransactionServiceBeanTest.java
@@ -8,7 +8,7 @@ import org.iana.rzm.facade.system.trans.SystemTransactionService;
 import org.iana.rzm.facade.system.trans.TransactionVO;
 import org.iana.rzm.user.RZMUser;
 import org.iana.rzm.user.SystemRole;
-import org.iana.rzm.user.dao.UserDAO;
+import org.iana.rzm.user.UserManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
@@ -17,23 +17,26 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
 
 import java.util.List;
 
 public class SystemTransactionServiceBeanTest {
 
     private PlatformTransactionManager txMgr;
-    private UserDAO userDAO;
+    private UserManager userManager;
     private TransactionDefinition txDef = new DefaultTransactionDefinition();
     private SystemTransactionService service;
     private AuthenticationService authService;
+
+    private final static String USER_NAME = "test";
 
 
     @BeforeClass
     public void init() {
         ApplicationContext appCtx = SpringApplicationContext.getInstance().getContext();
         txMgr = (PlatformTransactionManager) appCtx.getBean("transactionManager");
-        userDAO = (UserDAO) appCtx.getBean("userDAO");
+        userManager = (UserManager) appCtx.getBean("userManager");
         service = (SystemTransactionService) appCtx.getBean("GuardedSystemTransactionService");
         authService = (AuthenticationService) appCtx.getBean("authenticationServiceBean");
         createTestUsers();
@@ -43,7 +46,7 @@ public class SystemTransactionServiceBeanTest {
     private void createTestUsers() {
         TransactionStatus txStatus = txMgr.getTransaction(txDef);
         RZMUser testUser = createTestUser();
-        userDAO.create(testUser);
+        userManager.create(testUser);
         txMgr.commit(txStatus);
     }
 
@@ -53,7 +56,7 @@ public class SystemTransactionServiceBeanTest {
         testUser.setObjId(1L);
         testUser.setFirstName("TestUser");
         testUser.setLastName("Testuser");
-        testUser.setLoginName("test");
+        testUser.setLoginName(USER_NAME);
         testUser.setPassword("test");
         testUser.addRole(new SystemRole(SystemRole.SystemType.TC));
         testUser.setSecurID(false);
@@ -73,4 +76,9 @@ public class SystemTransactionServiceBeanTest {
         Assert.assertNotNull(list);
     }
 
+    @AfterClass
+    public void cleanUp() {
+        RZMUser user = userManager.get(USER_NAME);
+        userManager.delete(user);
+    }
 }
