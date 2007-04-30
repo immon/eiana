ae0132e89194137b7fb0e4f26aaae70b3f9559c9
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-04-30 08:06:31 +0000

    New dao structure with criteria-based find method.

---
+++ common-dao/test/org/iana/dao/hibernate/A.java
@@ -0,0 +1,12 @@
+package org.iana.dao.hibernate;
+
+import java.sql.Timestamp;
+
+/**
+ * @author Patrycja Wegrzynowicz
+ */
+public class A {
+    public String astr;
+    public Timestamp atmsp;
+    public B object;
+}
