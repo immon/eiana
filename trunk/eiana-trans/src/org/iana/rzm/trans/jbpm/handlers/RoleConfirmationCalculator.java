d6c68a8b11a4bdbb4f0b8e2478fab58bbae35d71
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-05-07 08:31:48 +0000

    role confirmation calculator initially added

---
+++ trunk/eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/RoleConfirmationCalculator.java
@@ -0,0 +1,44 @@
+package org.iana.rzm.trans.jbpm.handlers;
+
+import org.iana.rzm.trans.TransactionData;
+import org.iana.rzm.trans.confirmation.RoleConfirmation;
+import org.iana.rzm.trans.confirmation.StateConfirmations;
+import org.iana.rzm.user.AdminRole;
+import org.jbpm.configuration.ConfigurationException;
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.def.Node;
+import org.jbpm.graph.exe.ExecutionContext;
+import org.jbpm.graph.exe.Token;
+
+import java.util.List;
+
+/**
+ * @author Jakub Laszkiewicz
+ */
+public class RoleConfirmationCalculator implements ActionHandler {
+    private static final String ROLE_IANA = "IANA";
+    private static final String ROLE_DOC = "USDoC";
+
+    List<String> roles;
+
+    private AdminRole.AdminType getType(String role) {
+        if (ROLE_IANA.equals(role)) return AdminRole.AdminType.IANA;
+        if (ROLE_DOC.equals(role)) return AdminRole.AdminType.GOV_OVERSIGHT;
+        throw new IllegalArgumentException("role");
+    }
+
+    public void execute(ExecutionContext executionContext) throws Exception {
+        if (roles == null || roles.isEmpty())
+            throw new ConfigurationException("no roles specified");
+
+        TransactionData td = (TransactionData) executionContext.getContextInstance().getVariable("TRANSACTION_DATA");
+
+        StateConfirmations sc = new StateConfirmations();
+        for (String role : roles)
+            sc.addConfirmation(new RoleConfirmation(new AdminRole(getType(role))));
+
+        Token token = executionContext.getProcessInstance().getRootToken();
+        Node node = token.getNode();
+        td.setStateConfirmations(node.getName(), sc);
+    }
+}
