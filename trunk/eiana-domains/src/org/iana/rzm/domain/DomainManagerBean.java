3f93c8812aa6fd69f77ae53223b23227925622b7
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-05-07 11:20:47 +0000

    domain manager update method modified to handle existing name servers properly

--- trunk/eiana-domains/src/org/iana/rzm/domain/DomainManagerBean.java
+++ trunk/eiana-domains/src/org/iana/rzm/domain/DomainManagerBean.java
@@ -5,6 +5,7 @@ import org.iana.rzm.common.validators.CheckTool;
 import org.iana.criteria.Criterion;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author Patrycja Wegrzynowicz
@@ -12,10 +13,13 @@ import java.util.List;
 public class DomainManagerBean implements DomainManager {
 
     private DomainDAO dao;
+    private HostManager hostManager;
 
-    public DomainManagerBean(DomainDAO dao) {
+    public DomainManagerBean(DomainDAO dao, HostManager hostManager) {
         CheckTool.checkNull(dao, "domain dao");
         this.dao = dao;
+        CheckTool.checkNull(dao, "host manager");
+        this.hostManager = hostManager;
     }
 
     public Domain get(String name) {
@@ -31,6 +35,19 @@ public class DomainManagerBean implements DomainManager {
     }
 
     public void update(Domain domain) {
+        List<Host> newHosts = new ArrayList<Host>();
+
+        for (Host host : domain.getNameServers())
+            if (host.getObjId() == null) {
+                Host existing = hostManager.get(host.getName());
+                if (existing != null) {
+                    for (IPAddress ip : host.getAddresses())
+                        existing.addIPAddress(ip);
+                    newHosts.add(existing);
+                } else newHosts.add(host);
+            } else newHosts.add(host);
+
+        domain.setNameServers(newHosts);
         dao.update(domain);
     }
 
