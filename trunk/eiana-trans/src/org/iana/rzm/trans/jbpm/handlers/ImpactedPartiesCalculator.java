c76a3555066ca2fc8e6af73aa0de03460d435670
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-05-07 09:20:59 +0000

    fake implementation

--- trunk/eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/ImpactedPartiesCalculator.java
+++ trunk/eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/ImpactedPartiesCalculator.java
@@ -1,7 +1,11 @@
 package org.iana.rzm.trans.jbpm.handlers;
 
+import org.iana.rzm.trans.TransactionData;
+import org.iana.rzm.trans.confirmation.StateConfirmations;
 import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.def.Node;
 import org.jbpm.graph.exe.ExecutionContext;
+import org.jbpm.graph.exe.Token;
 
 /**
  * @author Patrycja Wegrzynowicz
@@ -9,6 +13,9 @@ import org.jbpm.graph.exe.ExecutionContext;
 public class ImpactedPartiesCalculator implements ActionHandler {
 
     public void execute(ExecutionContext executionContext) throws Exception {
-        //todo
+        TransactionData td = (TransactionData) executionContext.getContextInstance().getVariable("TRANSACTION_DATA");
+        Token token = executionContext.getProcessInstance().getRootToken();
+        Node node = token.getNode();
+        td.setStateConfirmations(node.getName(), new StateConfirmations());
     }
 }
