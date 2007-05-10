5ad34d10b22cc1008e4953817dafb95f92381a49
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-05-10 08:08:58 +0000

    new name validator

--- eiana-util/src/org/iana/rzm/common/exceptions/InvalidNameException.java
+++ eiana-util/src/org/iana/rzm/common/exceptions/InvalidNameException.java
@@ -3,12 +3,18 @@ package org.iana.rzm.common.exceptions;
 public class InvalidNameException extends RuntimeException {
 
     String name;
+    String reason;
 
-    public InvalidNameException(String name) {
+    public InvalidNameException(String name, String reason) {
         this.name = name;
+        this.reason = reason;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public String getReason() {
+        return this.reason;
     }
 }
