549d5b41a8049894f0350b0c6de48ad4a877985a
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-05-24 15:21:45 +0000

    users generation added

--- trunk/eiana-init/src/org/iana/rzm/init/ant/InitDatabaseFromXMLTask.java
+++ trunk/eiana-init/src/org/iana/rzm/init/ant/InitDatabaseFromXMLTask.java
@@ -2,8 +2,13 @@ package org.iana.rzm.init.ant;
 
 import org.iana.rzm.domain.*;
 import org.iana.rzm.common.exceptions.InvalidIPAddressException;
+import org.iana.rzm.common.exceptions.InvalidNameException;
 import org.iana.rzm.init.ant.decorators.DomainRegistryDecorator;
 import org.iana.rzm.init.ant.decorators.DomainDecorator;
+import org.iana.rzm.user.RZMUser;
+import org.iana.rzm.user.MD5Password;
+import org.iana.rzm.user.SystemRole;
+import org.iana.rzm.user.AdminRole;
 import org.hibernate.Session;
 
 import java.net.MalformedURLException;
@@ -29,11 +34,79 @@ public class InitDatabaseFromXMLTask extends HibernateTask {
     }
     public void doExecute(Session session) throws MalformedURLException, NameServerAlreadyExistsException, InvalidIPAddressException, DynaXMLException, FileNotFoundException, UnsupportedEncodingException {
         DomainManager domainManager = (DomainManager) SpringInitContext.getContext().getBean("domainManager");
+
+        RZMUser iana = setupUser(new RZMUser(), "iana");
+        iana.addRole(new AdminRole(AdminRole.AdminType.IANA));
+        session.save(iana);
+
+        RZMUser govOversight = setupUser(new RZMUser(), "gov_oversight");
+        govOversight.addRole(new AdminRole(AdminRole.AdminType.GOV_OVERSIGHT));
+        session.save(govOversight);
+
+        RZMUser zonePublisher = setupUser(new RZMUser(), "zone_publisher");
+        zonePublisher.addRole(new AdminRole(AdminRole.AdminType.ZONE_PUBLISHER));
+        session.save(zonePublisher);
+
         for (DomainDecorator domainDecorator : getDomainsFromXML()) {
-            System.out.print("\n     ----- TLD: " + domainDecorator.getDomain().getName() + " -----\n");
+            String domain = domainDecorator.getDomain().getName();
+            System.out.print("\n     ----- TLD: " + domain + " -----\n");
+
+            session.save(setupSystemUser(domain + "-ac1", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.AC), domain, true, true, true)));
+            session.save(setupSystemUser(domain + "-ac2", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.AC), domain, true, false, true)));
+            session.save(setupSystemUser(domain + "-ac3", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.AC), domain, false, false, true)));
+
+            session.save(setupSystemUser(domain + "-so1", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.SO), domain, true, true, true)));
+            session.save(setupSystemUser(domain + "-so2", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.SO), domain, true, false, true)));
+            session.save(setupSystemUser(domain + "-so3", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.SO), domain, false, false, true)));
+
+            session.save(setupSystemUser(domain + "-tc1", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.TC), domain, true, true, true)));
+            session.save(setupSystemUser(domain + "-tc2", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.TC), domain, true, false, true)));
+            session.save(setupSystemUser(domain + "-tc3", setupSystemRole(
+                    new SystemRole(SystemRole.SystemType.TC), domain, false, false, true)));
+
             domainManager.create(domainDecorator.getDomain());
         }
-        System.out.print("\n ALL OK");
+    }
+
+    private RZMUser setupUser(RZMUser user, String name) {
+        user.setEmail(name + "-@no-mail.org");
+        user.setFirstName(name);
+        user.setLastName(name);
+        user.setLoginName(name);
+        user.setOrganization(name);
+        user.setPassword(new MD5Password(name));
+        user.setSecurID(false);
+        return user;
+    }
+
+    private SystemRole setupSystemRole(SystemRole role, String name, boolean acceptFrom,
+                                       boolean mustAccept, boolean notify) throws InvalidNameException {
+        role.setName(name);
+        role.setAcceptFrom(acceptFrom);
+        role.setMustAccept(mustAccept);
+        role.setNotify(notify);
+        return role;
+    }
+
+    private RZMUser setupSystemUser(String name, SystemRole role) {
+        RZMUser user = new RZMUser();
+        user.setEmail(name + "@no-mail.org");
+        user.setFirstName(name + " first name");
+        user.setLastName(name + " last name");
+        user.setLoginName(name);
+        user.setOrganization(name + " organization");
+        user.setPassword(new MD5Password(name + "password"));
+        user.setSecurID(false);
+        user.addRole(role);
+        return user;
     }
 
     public static void main(String[] args) throws Exception {
@@ -42,6 +115,7 @@ public class InitDatabaseFromXMLTask extends HibernateTask {
         InitDatabaseFromXMLTask task = new InitDatabaseFromXMLTask();
         task.setAnnotationConfiguration("hibernate.cfg.xml");
         task.execute();
+        System.out.print("\n\n ALL OK \n\n");
     }
 
     private Reader createReader(String filename) throws UnsupportedEncodingException {
