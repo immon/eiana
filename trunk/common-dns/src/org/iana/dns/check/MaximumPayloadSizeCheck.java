53f165ff93710600b2411d3349cefeae772f501e
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-06-06 08:35:23 +0000

    new check maximum payload size and exception todo

---
+++ trunk/common-dns/src/org/iana/dns/check/MaximumPayloadSizeCheck.java
@@ -0,0 +1,14 @@
+package org.iana.dns.check;
+
+/**
+ * @author Piotr Tkaczyk
+ *         <p/>
+ *         (Test 2)
+ *         Checks that estimated response size from root server is not greater than 512 bytes.
+ */
+public class MaximumPayloadSizeCheck extends NameServerCheckBase {
+
+    void doCheck(DNSNameServer ns) throws DNSTechnicalCheckException {
+        //todo
+    }
+}
