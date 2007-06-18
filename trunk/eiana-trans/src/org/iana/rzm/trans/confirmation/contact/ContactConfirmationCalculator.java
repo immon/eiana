293450ac9f401bc0b05e6608bf094517aba87f5d
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-06-18 12:21:11 +0000

    feature: proper author tag added

--- trunk/eiana-trans/src/org/iana/rzm/trans/confirmation/contact/ContactConfirmationCalculator.java
+++ trunk/eiana-trans/src/org/iana/rzm/trans/confirmation/contact/ContactConfirmationCalculator.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * @author Jakub Laszkiewicz
  * @author Patrycja Wegrzynowicz
  */
 public class ContactConfirmationCalculator implements ActionHandler {
