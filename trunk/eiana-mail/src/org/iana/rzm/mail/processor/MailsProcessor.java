0290d51c6edda4a71c67e3956443edfce683da6c
jlaszk <jlaszk@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-07-10 14:25:53 +0000

    not supported non-textual message handling added to mail processor

--- trunk/eiana-mail/src/org/iana/rzm/mail/processor/MailsProcessor.java
+++ trunk/eiana-mail/src/org/iana/rzm/mail/processor/MailsProcessor.java
@@ -1,8 +1,11 @@
 package org.iana.rzm.mail.processor;
 
+import javax.mail.internet.MimeMessage;
+
 /**
  * @author Jakub Laszkiewicz
  */
 public interface MailsProcessor {
     public void process(String from, String subject, String content) throws MailsProcessorException;
+    public void process(MimeMessage message) throws MailsProcessorException;
 }
