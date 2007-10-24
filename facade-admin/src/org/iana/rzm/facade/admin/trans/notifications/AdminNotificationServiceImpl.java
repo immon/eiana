90957e757e2a761a23a87140db2473ff7e8c48f0
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-10-24 07:35:40 +0000

    fix: notification service with optional email

--- facade-admin/src/org/iana/rzm/facade/admin/trans/notifications/AdminNotificationServiceImpl.java
+++ facade-admin/src/org/iana/rzm/facade/admin/trans/notifications/AdminNotificationServiceImpl.java
@@ -1,26 +1,25 @@
 package org.iana.rzm.facade.admin.trans.notifications;
 
-import org.iana.rzm.facade.system.notification.NotificationVO;
-import org.iana.rzm.facade.system.notification.NotificationConverter;
-import org.iana.rzm.facade.system.notification.NotificationAddresseeVO;
-import org.iana.rzm.facade.system.notification.NotificationCriteriaConverter;
-import org.iana.rzm.facade.services.AbstractRZMStatefulService;
-import org.iana.rzm.facade.admin.trans.FacadeTransactionException;
-import org.iana.rzm.common.exceptions.InfrastructureException;
-import org.iana.rzm.common.validators.CheckTool;
-import org.iana.rzm.trans.Transaction;
-import org.iana.rzm.trans.TransactionState;
-import org.iana.rzm.trans.NoSuchTransactionException;
-import org.iana.rzm.trans.TransactionManager;
-import org.iana.rzm.trans.confirmation.contact.ContactIdentity;
-import org.iana.rzm.trans.confirmation.Confirmation;
-import org.iana.rzm.user.UserManager;
+import org.iana.criteria.And;
 import org.iana.criteria.Criterion;
 import org.iana.criteria.Equal;
 import org.iana.criteria.In;
-import org.iana.criteria.And;
 import org.iana.notifications.*;
 import org.iana.notifications.exception.NotificationException;
+import org.iana.rzm.common.exceptions.InfrastructureException;
+import org.iana.rzm.facade.admin.trans.FacadeTransactionException;
+import org.iana.rzm.facade.services.AbstractRZMStatefulService;
+import org.iana.rzm.facade.system.notification.NotificationAddresseeVO;
+import org.iana.rzm.facade.system.notification.NotificationConverter;
+import org.iana.rzm.facade.system.notification.NotificationCriteriaConverter;
+import org.iana.rzm.facade.system.notification.NotificationVO;
+import org.iana.rzm.trans.NoSuchTransactionException;
+import org.iana.rzm.trans.Transaction;
+import org.iana.rzm.trans.TransactionManager;
+import org.iana.rzm.trans.TransactionState;
+import org.iana.rzm.trans.confirmation.Confirmation;
+import org.iana.rzm.trans.confirmation.contact.ContactIdentity;
+import org.iana.rzm.user.UserManager;
 
 import java.util.*;
 
@@ -128,7 +127,10 @@ public class AdminNotificationServiceImpl extends AbstractRZMStatefulService imp
 
     public void resendNotification(long transactionId, NotificationVO.Type type, String comment, String addresseeEmail) throws InfrastructureException, FacadeTransactionException {
         isUserInRole();
-        if (addresseeEmail == null) resendNotification(transactionId, type, comment);
+        if (addresseeEmail == null) {
+            resendNotification(transactionId, type, comment);
+            return;
+        }
         try {
             Set<Addressee> addressee = new HashSet<Addressee>();
             addressee.add(new EmailAddressee(addresseeEmail, ""));
