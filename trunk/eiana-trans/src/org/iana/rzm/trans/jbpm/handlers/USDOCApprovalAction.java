1802497dbe8ad62cbf5e14278024080be1deb8f6
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2007-11-29 22:29:35 +0000

    feature: change summary in USDoC template

--- trunk/eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/USDOCApprovalAction.java
+++ trunk/eiana-trans/src/org/iana/rzm/trans/jbpm/handlers/USDOCApprovalAction.java
@@ -4,6 +4,7 @@ import org.iana.epp.EPPClient;
 import org.iana.rzm.domain.HostManager;
 import org.iana.rzm.trans.Transaction;
 import org.iana.rzm.trans.TransactionData;
+import org.iana.rzm.trans.change.DomainChangePrinter;
 import org.iana.rzm.trans.epp.EPPChangeRequest;
 import org.jbpm.graph.exe.ExecutionContext;
 
@@ -23,6 +24,8 @@ public class USDOCApprovalAction extends ActionExceptionHandler {
             notifier.setEppID(rsp[0]);
             notifier.setNotification("usdoc-confirmation-nschange");
         } else {
+            TransactionData td = (TransactionData) executionContext.getContextInstance().getVariable("TRANSACTION_DATA");
+            notifier.setReceipt(DomainChangePrinter.print(td.getDomainChange()));
             notifier.setNotification("usdoc-confirmation");
         }
         notifier.execute(executionContext);
