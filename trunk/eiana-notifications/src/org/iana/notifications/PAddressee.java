0bb763cbfe52bff90194826a5d6b85c5ecc17875
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2008-02-21 14:55:19 +0000

    multiple addressees with the same name & email support, impacted parties resend option added

--- trunk/eiana-notifications/src/org/iana/notifications/PAddressee.java
+++ trunk/eiana-notifications/src/org/iana/notifications/PAddressee.java
@@ -52,23 +52,4 @@ public class PAddressee implements Serializable {
         this.email = email;
     }
 
-
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        PAddressee that = (PAddressee) o;
-
-        if (email != null ? !email.equals(that.email) : that.email != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-
-        return true;
-    }
-
-    public int hashCode() {
-        int result;
-        result = (name != null ? name.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-        return result;
-    }
 }
--- trunk/facade-admin/src/org/iana/rzm/facade/admin/trans/notifications/AdminNotificationService.java
+++ trunk/facade-admin/src/org/iana/rzm/facade/admin/trans/notifications/AdminNotificationService.java
@@ -13,17 +13,31 @@ import java.util.List;
  */
 public interface AdminNotificationService extends RZMStatefulService {
 
+    /**
+     * Returns a list of persistent notifications for a given transaction.
+     *
+     * @param transactionId the transaction identifier
+     * @return a list of persistent notifications; empty list if none notification has been found.
+     *
+     * @throws InfrastructureException when resending notification failed.
+     */
     public List<NotificationVO> getNotifications(long transactionId) throws InfrastructureException;
 
     /**
-     * Redends confirmation notifiactions of given <code>type</code> for the transaction identified
-     * by given <code>transactionId</code> to addressees, who did not confirmed yet.
-     * Notification body content is preceded with given <code>comment</comment>.
-     * @param transactionId identifier of the transaction for which confirmation notifiactions
-     * have to be resent.
-     * @param type of the notifications to be resent.
-     * @see NotificationVO.Type
-     * @param comment to be added at the beginning of the notification body content.
+     * Re-sends a notification of a given type already sent in a context of a given transaction.
+     * The notification is re-sent only to those who have not confirmed it yet; an additional email addressee
+     * can be specified. The subject and body of the notification remains the same but in case of a body it's possible to add
+     * a comment which is added at a top of it.
+     *
+     * @param transactionId the identifier of the transaction in a context of which the notification to be re-sent was
+     * produced.
+     * @param type the type of the notification to re-sent.
+     * @param comment the comment to be added at a top of the notification body; if null no comment is added and the body
+     * of the resent notification is the same as the original one.
+     * @param email an additional email to which the new notifiaction is to be sent as well (in addition to the addressees
+     * which have not confirmed yet).
+     *
+     *
      * @throws InfrastructureException when resending notification failed.
      */
     public void resendNotification(long transactionId, NotificationVO.Type type, String comment,  String email) throws InfrastructureException, FacadeTransactionException;
