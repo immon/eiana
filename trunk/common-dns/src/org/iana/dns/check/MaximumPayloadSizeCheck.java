904ad52a8c99c5698e06c8eadfd6bca3440b8b72
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2008-02-25 11:30:22 +0000

    fix: check updated

--- trunk/common-dns/src/org/iana/dns/check/MaximumPayloadSizeCheck.java
+++ trunk/common-dns/src/org/iana/dns/check/MaximumPayloadSizeCheck.java
@@ -2,12 +2,11 @@ package org.iana.dns.check;
 
 import org.iana.dns.DNSDomain;
 import org.iana.dns.DNSIPAddress;
-import static org.iana.dns.DNSIPAddress.Type.IPv4;
 import org.iana.dns.check.exceptions.MaximumPayloadSizeExceededException;
 
-import java.util.Set;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * (Test 2)
@@ -17,55 +16,61 @@ import java.util.ArrayList;
  */
 public class MaximumPayloadSizeCheck implements DNSDomainTechnicalCheck {
 
-    private static final int SZ_HEADER = 12;
-    private static final int SZ_QUERY = 7;
+    private static final int MAX_SIZE = 512;
+    private static final int NS_SIZE = 12;
+    private static final int HEADER_SIZE = 12;
+    private static final int QUERY_SIZE = 255;
+    private static final int A_SIZE = 16;
+    private static final int AAAA_SIZE = 28;
+
     private static final int SZ_PTR = 2;
-    private static final int SZ_TYPE = 2;
-    private static final int SZ_CLASS = 2;
-    private static final int SZ_TTL = 4;
-    private static final int SZ_RDLEN = 2;
 
     private List<String> suffixes = new ArrayList<String>();
 
     public void check(DNSDomain domain, Set<DNSNameServer> nameServers) throws DNSTechnicalCheckException {
+        int ns_set_size = 0;
+        boolean needs_v4_glue = false;
+        boolean needs_v6_glue = false;
 
-        int domainNameSize = domain.getName().length() + 2;
+        compress_domain(domain.getName());
 
-        int estimatedSize = SZ_HEADER + SZ_QUERY + domainNameSize;
-        int constSize = SZ_PTR + SZ_TYPE + SZ_CLASS + SZ_TTL + SZ_RDLEN;
-        suffixes.add("." + domain.getName());
-
-        //section 0 of SOA Record
-        estimatedSize += domainNameSize;
-        //section 1
-        estimatedSize += domainNameSize;
         for (DNSNameServer ns : nameServers) {
-            int nsNameSize = nsNameEstimatedSize(ns.getName());
-            //section 2
-            estimatedSize += nsNameSize + constSize;
-            //section 3
+            int domainLen = compress_domain(ns.getName());
+            ns_set_size += NS_SIZE + domainLen;
+
             for (DNSIPAddress ipAddress : ns.getIPAddresses()) {
-                int ipAddressSize = (ipAddress.getType() == IPv4) ? 8 : 16;
-                estimatedSize += ipAddressSize + SZ_TYPE + SZ_TTL + SZ_PTR;
+                if (DNSIPAddress.Type.IPv4.equals(ipAddress.getType())) {
+                    if (in_bailiwick(ns.getName(), domain.getName()))
+                        needs_v4_glue = true;
+                } else {
+                    if (in_bailiwick(ns.getName(), domain.getName()))
+                        needs_v6_glue = true;
+                }
             }
         }
-        if (estimatedSize > 512) throw new MaximumPayloadSizeExceededException(domain, estimatedSize);
+
+        int packet_size = HEADER_SIZE + QUERY_SIZE + ns_set_size;
+
+        if (needs_v4_glue)
+            packet_size += A_SIZE;
+        if (needs_v6_glue)
+            packet_size += AAAA_SIZE;
+        
+        if (packet_size > MAX_SIZE) throw new MaximumPayloadSizeExceededException(domain, packet_size);
     }
 
-    private int nsNameEstimatedSize(String nsName) {
-        int len = nsName.length() + 2;
+    private int compress_domain(String nsName) {
         String[] labels = nsName.split("\\.");
 
         for (int i=0; i<labels.length; i++) {
             String suffix = joinLabels(i, labels);
-
             if (suffixes.contains(suffix))
-                return nsName.length() - suffix.length() + SZ_PTR;
+                return (nsName.length() - (suffix.length() -1)) + SZ_PTR;
 
             suffixes.add(suffix);
         }
 
-        return len;
+        return nsName.length() + 2;
     }
 
     private String joinLabels(int from, String[] labels) {
@@ -76,5 +81,15 @@ public class MaximumPayloadSizeCheck implements DNSDomainTechnicalCheck {
         }
 
         return buff.toString();
+    }
+
+    private boolean in_bailiwick(String child, String parent) {
+        if (parent.length() == 0) return true;
+
+        int childLen = child.length();
+        if (child.substring(childLen - parent.length(), childLen).equals(parent))
+            return true;
+        else
+            return false;
     }
 }
