4ca66adab8e3b017498cd03d9036904d14c4e843
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2008-04-04 14:26:21 +0000

    feature: logging all transaction emails to RT

--- trunk/eiana-mail/src/org/iana/rzm/mail/processor/verisign/VeriSignMailParser.java
+++ trunk/eiana-mail/src/org/iana/rzm/mail/processor/verisign/VeriSignMailParser.java
@@ -19,22 +19,17 @@ public class VeriSignMailParser implements EmailParser {
 
     private RegexParser subjectPattern;
 
-    private RegexParser contentPattern;
-
     public VeriSignMailParser(String verisignAddress, RegexParser subjectPattern, RegexParser contentPattern) {
         CheckTool.checkNull(verisignAddress, "verisign address");
         CheckTool.checkNull(subjectPattern, "subject pattern");
-        CheckTool.checkNull(contentPattern, "content pattern");
         this.verisignAddress = verisignAddress;
         this.subjectPattern = subjectPattern;
-        this.contentPattern = contentPattern;
     }
 
     public MessageData parse(String from, String subject, String content) throws EmailParseException {
         if (!verisignAddress.equals(from)) throw new EmailParseException("The from address [" + from + "] does not match the verisign address [" + verisignAddress + "]");
         try {
             RegexParser.Tokens subjectTokens = parseSubject(subject);
-            RegexParser.Tokens contentTokens = parseContent(content);
             long ticketID = Long.parseLong(subjectTokens.token(TICKET_ID));
             return new VeriSignMail(ticketID);
         } catch (NumberFormatException e) {
@@ -47,14 +42,6 @@ public class VeriSignMailParser implements EmailParser {
             return subjectPattern.parse(subject);
         } catch (ParseException e) {
             throw new EmailParseException("Subject does not match contact confirmation subject pattern", e);
-        }
-    }
-
-    private RegexParser.Tokens parseContent(String content) throws EmailParseException {
-        try {
-            return contentPattern.parse(content);
-        } catch (ParseException e) {
-            throw new EmailParseException("Content does not match contact confirmation content pattern", e);
         }
     }
 
