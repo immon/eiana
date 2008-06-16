3f545682536ab899cce3974b04d59eec2dbb61be
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2008-06-16 13:01:53 +0000

    serial number set in transactionData in zonePendingTesting state

--- eiana-trans/src/org/iana/rzm/trans/process/pending_zone_testing/ZoneTestingAction.java
+++ eiana-trans/src/org/iana/rzm/trans/process/pending_zone_testing/ZoneTestingAction.java
@@ -2,8 +2,11 @@ package org.iana.rzm.trans.process.pending_zone_testing;
 
 import org.apache.log4j.Logger;
 import org.iana.dns.DNSDomain;
+import org.iana.dns.obj.DNSHostImpl;
+import org.iana.dns.obj.DNSDomainImpl;
 import org.iana.dns.check.DNSTechnicalCheck;
 import org.iana.dns.check.DNSTechnicalCheckException;
+import org.iana.dns.check.DNSNameServer;
 import org.iana.objectdiff.ChangeApplicator;
 import org.iana.objectdiff.DiffConfiguration;
 import org.iana.objectdiff.ObjectChange;
@@ -14,6 +17,8 @@ import org.iana.rzm.trans.dns.DNSConverter;
 import org.iana.rzm.trans.process.general.handlers.ActionExceptionHandler;
 import org.jbpm.configuration.ObjectFactory;
 import org.jbpm.graph.exe.ExecutionContext;
+
+import java.util.List;
 
 /**
  * @author Jakub Laszkiewicz
@@ -26,7 +31,8 @@ public class ZoneTestingAction extends ActionExceptionHandler {
         ObjectFactory objectFactory = executionContext.getJbpmContext().getObjectFactory();
         DomainManager domainManager = (DomainManager) objectFactory.createObject("domainManager");
         DiffConfiguration diffConfig = (DiffConfiguration) objectFactory.createObject("diffConfig");
-        DNSTechnicalCheck zoneCheck = (DNSTechnicalCheck) objectFactory.createObject("zoneTestingCheck"); 
+        DNSTechnicalCheck zoneCheck = (DNSTechnicalCheck) objectFactory.createObject("zoneTestingCheck");
+        List<DNSHostImpl> rootServers = (List) objectFactory.createObject("rootServersList");
 
         Domain retrievedDomain = domainManager.get(td.getCurrentDomain().getName()).clone();
         ObjectChange change = td.getDomainChange();
@@ -35,8 +41,16 @@ public class ZoneTestingAction extends ActionExceptionHandler {
 
         DNSDomain dnsDomain = DNSConverter.toDNSDomain(retrievedDomain);
 
+        long serialNumber = -1;
+        for (DNSHostImpl rootServer : rootServers) {
+            DNSNameServer dnsNameServer = new DNSNameServer(new DNSDomainImpl("."), rootServer, 1);
+            serialNumber = dnsNameServer.getSerialNumber();
+            if (serialNumber > -1) break;
+        }
+
         try {
             zoneCheck.check(dnsDomain);
+            td.setSerialNumber("" + serialNumber);
             executionContext.leaveNode("accept");
         } catch (DNSTechnicalCheckException e) {
             Logger.getLogger(ZoneTestingAction.class).warn(e.getMessage());
