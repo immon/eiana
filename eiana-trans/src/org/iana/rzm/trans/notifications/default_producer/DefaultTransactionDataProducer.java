6bed4bd3c73988b54ebaa0e9b3511f4eee1d1e75
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2008-06-27 14:56:39 +0000

    fix for bug 2001317 (wrong date)

--- eiana-trans/src/org/iana/rzm/trans/notifications/default_producer/DefaultTransactionDataProducer.java
+++ eiana-trans/src/org/iana/rzm/trans/notifications/default_producer/DefaultTransactionDataProducer.java
@@ -1,11 +1,15 @@
 package org.iana.rzm.trans.notifications.default_producer;
 
-import org.iana.notifications.producers.*;
-import org.iana.rzm.domain.*;
-import org.iana.rzm.trans.*;
+import org.iana.notifications.producers.DataProducer;
+import org.iana.rzm.domain.Domain;
+import org.iana.rzm.trans.TransactionData;
+import org.iana.rzm.trans.TransactionState;
+import org.iana.rzm.trans.TransactionStateLogEntry;
 
-import java.util.*;
 import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author Piotr Tkaczyk
@@ -18,15 +22,22 @@ public class DefaultTransactionDataProducer implements DataProducer {
         Domain currentDomain = td.getCurrentDomain();
         values.put("domainName", currentDomain.getFqdnName());
         values.put("ticket", td.getTicketID().toString());
+        values.put("requestDate", String.valueOf(td.getCreated().getTime()));
         String period = (String) dataSource.get("period");
         if (period != null && period.trim().length() > 0) {
-            long time = td.getCreated().getTime();
-            long intPeriod = Integer.parseInt(period);
-            time += (intPeriod * 24 * 60 * 60 * 1000);
-            values.put("requestDate", String.valueOf(time));
-        } else {
-            values.put("requestDate", String.valueOf(td.getCreated().getTime()));
+            TransactionStateLogEntry logEntry;
+            for(Iterator i = td.getStateLog().iterator(); i.hasNext();) {
+                logEntry = (TransactionStateLogEntry) i.next();
+                if (!i.hasNext() && logEntry != null) {
+                    long time = logEntry.getState().getEnd().getTime();
+                    long intPeriod = Integer.parseInt(period);
+                    time += (intPeriod * 24 * 60 * 60 * 1000);
+                    Object o = new Timestamp(time);
+                    values.put("requestDate", String.valueOf(time));
+                }
+            }
         }
+
         values.put("confirmationDate", td.getStateEndDate(TransactionState.Name.PENDING_CONTACT_CONFIRMATION));
         values.put("docVrsnDate", td.getStateStartDate(TransactionState.Name.PENDING_USDOC_APPROVAL));
         values.put("implementationDate", td.getStateEndDate(TransactionState.Name.PENDING_USDOC_APPROVAL));
