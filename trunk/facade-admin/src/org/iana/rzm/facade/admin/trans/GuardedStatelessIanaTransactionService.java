e329bbbbef1b303895b55cd84a5a75dca458d972
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2009-02-24 15:22:28 +0000

    usDoC is now able to log in as admin and view transactions in USDOC_APPROVAL or COMPLETED state

--- trunk/facade-admin/src/org/iana/rzm/facade/admin/trans/GuardedStatelessIanaTransactionService.java
+++ trunk/facade-admin/src/org/iana/rzm/facade/admin/trans/GuardedStatelessIanaTransactionService.java
@@ -20,32 +20,23 @@ import java.util.List;
 /**
  * @author Piotr Tkaczyk
  */
-public class GuardedStatelessAdminTransactionService extends AbstractRZMStatelessService implements StatelessAdminTransactionService {
+public class GuardedStatelessIanaTransactionService extends AbstractRZMStatelessService implements StatelessAdminTransactionService {
 
-    private StatelessAdminTransactionService statelessIanaTransactionService;
-    private StatelessAdminTransactionService statelessDoCTransactionService;
+    private StatelessAdminTransactionService statelessAdminTransactionService;
 
-    public GuardedStatelessAdminTransactionService(UserManager userManager,
-                                                   StatelessAdminTransactionService statelessIanaTransactionService,
-                                                   StatelessAdminTransactionService statelessDoCTransactionService) {
+    public GuardedStatelessIanaTransactionService(UserManager userManager, StatelessAdminTransactionService statelessAdminTransactionService) {
         super(userManager);
-        this.statelessIanaTransactionService = statelessIanaTransactionService;
-        this.statelessDoCTransactionService = statelessDoCTransactionService;
-    }
-
-    private StatelessAdminTransactionService getService(AuthenticatedUser user) {
-        if (checkIsIana(user))
-            return statelessIanaTransactionService;
-        else
-            return statelessDoCTransactionService;
+        this.statelessAdminTransactionService = statelessAdminTransactionService;
     }
 
     public String queryTransactionEPPStatus(long id, AuthenticatedUser authUser) throws NoObjectFoundException, InvalidEPPTransactionException, InfrastructureException, AccessDeniedException {
-        return getService(authUser).queryTransactionEPPStatus(id, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.queryTransactionEPPStatus(id, authUser);
     }
 
     public TransactionVO getTransaction(long id, AuthenticatedUser authUser) throws NoObjectFoundException, AccessDeniedException {
-        return getService(authUser).getTransaction(id, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.getTransaction(id, authUser);
     }
 
     public void transitTransactionToState(long id, TransactionStateVO.Name targetStateName, AuthenticatedUser authUser) throws NoSuchStateException, StateUnreachableException, NoObjectFoundException, FacadeTransactionException, AccessDeniedException {
@@ -53,112 +44,140 @@ public class GuardedStatelessAdminTransactionService extends AbstractRZMStateles
     }
 
     public void transitTransactionToState(long id, String targetStateName, AuthenticatedUser authUser) throws NoSuchStateException, StateUnreachableException, NoObjectFoundException, FacadeTransactionException, AccessDeniedException {
-        getService(authUser).transitTransactionToState(id, targetStateName, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.transitTransactionToState(id, targetStateName, authUser);
     }
 
     public void updateTransaction(long id, Long ticketId, boolean redelegation, String comment, AuthenticatedUser authUser) throws NoObjectFoundException, StateUnreachableException, InfrastructureException, AccessDeniedException {
-        getService(authUser).updateTransaction(id, ticketId, redelegation, comment, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.updateTransaction(id, ticketId, redelegation, comment, authUser);
     }
 
     public void updateTransaction(TransactionVO trans, AuthenticatedUser authUser) throws NoObjectFoundException, StateUnreachableException, InfrastructureException, AccessDeniedException {
-        getService(authUser).updateTransaction(trans, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.updateTransaction(trans, authUser);
     }
 
     public void moveTransactionToNextState(long id, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, InfrastructureException, org.iana.rzm.facade.system.trans.IllegalTransactionStateException {
-        getService(authUser).moveTransactionToNextState(id, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.moveTransactionToNextState(id, authUser);
     }
 
     public void rejectTransaction(long id, AuthenticatedUser authUser) throws NoObjectFoundException, AccessDeniedException, InfrastructureException {
-        getService(authUser).rejectTransaction(id, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.rejectTransaction(id, authUser);
     }
 
     public void transitTransaction(long id, String transitionName, AuthenticatedUser authUser) throws NoObjectFoundException, InfrastructureException, AccessDeniedException {
-        getService(authUser).transitTransaction(id, transitionName, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.transitTransaction(id, transitionName, authUser);
     }
 
     public void deleteTransaction(TransactionVO transactionVO, AuthenticatedUser authUser) throws NoObjectFoundException, AccessDeniedException {
-        getService(authUser).deleteTransaction(transactionVO, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.deleteTransaction(transactionVO, authUser);
     }
 
     public void deleteTransaction(long transactionId, AuthenticatedUser authUser) throws NoObjectFoundException, AccessDeniedException {
-        getService(authUser).deleteTransaction(transactionId, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.deleteTransaction(transactionId, authUser);
     }
 
+
     public TransactionVO get(long id, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, InfrastructureException {
-        return getService(authUser).get(id, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.get(id, authUser);
     }
 
     public void acceptTransaction(long id, String token, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, InfrastructureException {
-        getService(authUser).acceptTransaction(id, token, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.acceptTransaction(id, token, authUser);
     }
 
     public void rejectTransaction(long id, String token, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, InfrastructureException {
-        getService(authUser).rejectTransaction(id, token, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.rejectTransaction(id, token, authUser);
     }
 
     public List<TransactionVO> createTransactions(IDomainVO domain, boolean splitNameServerChange, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, NoDomainModificationException, InfrastructureException, InvalidCountryCodeException, TransactionExistsException, NameServerChangeNotAllowedException, SharedNameServersCollisionException, RadicalAlterationException {
-        return getService(authUser).createTransactions(domain, splitNameServerChange, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.createTransactions(domain, splitNameServerChange, authUser);
     }
 
     public List<TransactionVO> createTransactions(IDomainVO domain, boolean splitNameServerChange, String submitterEmail, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, NoDomainModificationException, InfrastructureException, InvalidCountryCodeException, TransactionExistsException, NameServerChangeNotAllowedException, SharedNameServersCollisionException, RadicalAlterationException {
-        return getService(authUser).createTransactions(domain, splitNameServerChange, submitterEmail, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.createTransactions(domain, splitNameServerChange, submitterEmail, authUser);
     }
 
     public List<TransactionVO> find(Order order, int offset, int limit, AuthenticatedUser authUser) throws AccessDeniedException, InfrastructureException {
-        return getService(authUser).find(order, offset, limit, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.find(order, offset, limit, authUser);
     }
 
     public List<TransactionVO> find(Criterion criteria, Order order, int offset, int limit, AuthenticatedUser authUser) throws AccessDeniedException, InfrastructureException {
-        return getService(authUser).find(criteria, order, offset, limit, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.find(criteria, order, offset, limit, authUser);
     }
 
     public List<TransactionVO> find(Criterion criteria, List<Order> order, int offset, int limit, AuthenticatedUser authUser) throws AccessDeniedException, InfrastructureException {
-        return getService(authUser).find(criteria, order, offset, limit, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.find(criteria, order, offset, limit, authUser);
     }
 
     public int count(Criterion criteria, AuthenticatedUser authUser) throws AccessDeniedException {
-        return getService(authUser).count(criteria, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.count(criteria, authUser);
     }
 
     public List<TransactionVO> find(Criterion criteria, int offset, int limit, AuthenticatedUser authUser) throws AccessDeniedException, InfrastructureException {
-        return getService(authUser).find(criteria, offset, limit, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.find(criteria, offset, limit, authUser);
     }
 
     public List<TransactionVO> find(Criterion criteria, AuthenticatedUser authUser) throws AccessDeniedException, InfrastructureException {
-        return getService(authUser).find(criteria, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.find(criteria, authUser);
     }
 
     public List<TransactionVO> createTransactions(IDomainVO domain, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, NoDomainModificationException, InfrastructureException, InvalidCountryCodeException, TransactionExistsException, NameServerChangeNotAllowedException, SharedNameServersCollisionException, RadicalAlterationException {
-        return getService(authUser).createTransactions(domain, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.createTransactions(domain, authUser);
     }
 
     public List<TransactionVO> createTransactions(IDomainVO domain, boolean splitNameServerChange, String submitterEmail, boolean performTechnicalCheck, String comment, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, NoDomainModificationException, InfrastructureException, InvalidCountryCodeException, DNSTechnicalCheckException, TransactionExistsException, NameServerChangeNotAllowedException, SharedNameServersCollisionException, RadicalAlterationException {
-        return getService(authUser).createTransactions(domain, splitNameServerChange, submitterEmail, performTechnicalCheck, comment, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.createTransactions(domain, splitNameServerChange, submitterEmail, performTechnicalCheck, comment, authUser);
     }
 
 
-    public void approveByUSDoC(long id, AuthenticatedUser authUser) throws NoObjectFoundException, IllegalTransactionStateException, AccessDeniedException, InfrastructureException {
-        getService(authUser).approveByUSDoC(id, authUser);
+    public void approveByUSDoC(long id, AuthenticatedUser authUser) throws NoObjectFoundException, org.iana.rzm.facade.system.trans.IllegalTransactionStateException, AccessDeniedException, InfrastructureException {
+        isIanaOrGOV(authUser);
+        statelessAdminTransactionService.approveByUSDoC(id, authUser);
     }
 
-    public void rejectByUSDoC(long id, AuthenticatedUser authUser) throws NoObjectFoundException, IllegalTransactionStateException, AccessDeniedException, InfrastructureException {
-        getService(authUser).rejectByUSDoC(id, authUser);
+    public void rejectByUSDoC(long id, AuthenticatedUser authUser) throws NoObjectFoundException, org.iana.rzm.facade.system.trans.IllegalTransactionStateException, AccessDeniedException, InfrastructureException {
+        isIanaOrGOV(authUser);
+        statelessAdminTransactionService.rejectByUSDoC(id, authUser);
     }
 
     public void withdrawTransaction(long id, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, TransactionCannotBeWithdrawnException, InfrastructureException {
-        getService(authUser).withdrawTransaction(id, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.withdrawTransaction(id, authUser);
     }
 
-    public void confirmByUSDoC(long id, boolean nsChange, boolean accept, AuthenticatedUser authUser) throws NoObjectFoundException, IllegalTransactionStateException, AccessDeniedException, InfrastructureException {
-        getService(authUser).confirmByUSDoC(id, nsChange, accept, authUser);
+    public void confirmByUSDoC(long id, boolean nsChange, boolean accept, AuthenticatedUser authUser) throws NoObjectFoundException, org.iana.rzm.facade.system.trans.IllegalTransactionStateException, AccessDeniedException, InfrastructureException {
+        isIanaOrGOV(authUser);
+        statelessAdminTransactionService.confirmByUSDoC(id, nsChange, accept, authUser);
     }
 
 
     public void acceptTransaction(long id, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, InfrastructureException {
-        getService(authUser).acceptTransaction(id, authUser);
+        isIana(authUser);
+        statelessAdminTransactionService.acceptTransaction(id, authUser);
     }
 
     public List<TransactionVO> getByTicketID(long id, AuthenticatedUser authUser) throws AccessDeniedException, NoObjectFoundException, InfrastructureException {
-        return getService(authUser).getByTicketID(id, authUser);
+        isIana(authUser);
+        return statelessAdminTransactionService.getByTicketID(id, authUser);
     }
 }
