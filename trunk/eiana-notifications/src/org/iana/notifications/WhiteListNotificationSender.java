edace08b2058469e7c3a655fc9f24f4f46800ce1
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2009-03-24 12:52:59 +0000

    New email sender bean "whiteListNotificationSender": puts through the emails with addressees on "white list" (configurable) otherwise (email not on "white list") replaces the addressee with the default one (configurable)

---
+++ trunk/eiana-notifications/src/org/iana/notifications/WhiteListNotificationSender.java
@@ -0,0 +1,56 @@
+package org.iana.notifications;
+
+import org.apache.log4j.Logger;
+import org.iana.rzm.common.validators.CheckTool;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Piotr Tkaczyk
+ */
+public class WhiteListNotificationSender implements NotificationSender {
+
+    private Set<String> whiteList;
+
+    private String defaultEmail;
+
+    private NotificationSender notificationSender;
+
+    private Logger logger = Logger.getLogger(WhiteListNotificationSender.class);
+
+    public WhiteListNotificationSender(NotificationSender notificationSender) {
+        CheckTool.checkNull(notificationSender, "notification sender");
+        this.notificationSender = notificationSender;
+    }
+
+    public void send(PNotification notification) throws NotificationSenderException {
+        Set<PAddressee> addressees = notification.getAddressees();
+        for (PAddressee addressee : addressees) {
+            if(!isOnWhiteList(addressee)) {
+                logger.debug("Email addreessee not on white list (" + addressee.getEmail() + " replaced to " + defaultEmail + ")");
+                addressee.setEmail(defaultEmail);
+            }
+        }
+
+        notificationSender.send(notification);
+    }
+
+    public void setWhiteList(List<String> whiteList) {
+        setWhiteList(new HashSet<String>(whiteList));
+    }
+
+    public void setWhiteList(Set<String> whiteList) {
+        CheckTool.checkNull(whiteList, "emails white list");
+        this.whiteList = whiteList;
+    }
+
+    public void setDefaultEmail(String defaultEmail) {
+        this.defaultEmail = defaultEmail;
+    }
+
+    private boolean isOnWhiteList(PAddressee addressee) {
+        return whiteList.contains(addressee.getEmail());
+    }
+}
