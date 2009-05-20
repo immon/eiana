cb6c1c31605658d5ef02789ae68549ddb46b08c8
piotr_tkaczyk <piotr_tkaczyk@54c49d9a-8729-0410-8bbb-d8f729217203>
2009-05-20 13:20:14 +0000

    NotUniqueIPAddressException otherHost added visitors (XML, Text) updated

--- trunk/common-dns/src/org/iana/dns/check/exceptions/NotUniqueIPAddressException.java
+++ trunk/common-dns/src/org/iana/dns/check/exceptions/NotUniqueIPAddressException.java
@@ -31,4 +31,23 @@ public class NotUniqueIPAddressException extends DomainTechnicalCheckException {
     public DNSHost getOtherHost() {
         return otherHost;
     }
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        NotUniqueIPAddressException that = (NotUniqueIPAddressException) o;
+
+        if (otherHost != null ? !otherHost.equals(that.otherHost) : that.otherHost != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (otherHost != null ? otherHost.hashCode() : 0);
+        return result;
+    }
 }
