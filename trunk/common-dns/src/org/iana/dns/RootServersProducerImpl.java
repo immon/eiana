b4b1f3273736627eb5340e32b24c0eab6ffdb353
pwegrzynowicz <pwegrzynowicz@54c49d9a-8729-0410-8bbb-d8f729217203>
2009-05-20 12:44:17 +0000

    root servers

--- trunk/common-dns/src/org/iana/dns/RootServersProducerImpl.java
+++ trunk/common-dns/src/org/iana/dns/RootServersProducerImpl.java
@@ -2,8 +2,10 @@ package org.iana.dns;
 
 import org.iana.config.Config;
 import org.iana.config.ParameterManager;
+import org.iana.config.Parameter;
 import org.iana.config.impl.ConfigException;
 import org.iana.config.impl.OwnedConfig;
+import org.iana.config.impl.SetParameter;
 import org.iana.dns.obj.DNSHostImpl;
 
 import java.util.ArrayList;
@@ -27,26 +29,27 @@ public class RootServersProducerImpl implements RootServersProducer {
     }
 
     public List<DNSHost> getRootServers() throws ConfigException {
+        Config rootServerConfig = config.getSubConfig(rootServerParamNames);
+        if (rootServerConfig == null)
+            throw new IllegalArgumentException("root servers subconfig is null");
 
-        Set<String> rootServerNames = config.getParameterSet(rootServerParamNames);
-
-        if (rootServerNames == null)
-            return rootServers;
-
-        List<DNSHost> dnsHostsList = new ArrayList<DNSHost>(rootServerNames.size());
-
-        for (String rootServer : rootServerNames) {
+        List<DNSHost> dnsHostsList = new ArrayList<DNSHost>();
+        for (String rootServer : rootServerConfig.getParameterNames()) {
             Set<String> serverAddresses = config.getParameterSet(rootServer);
             DNSHostImpl dnsHost = new DNSHostImpl(rootServer);
             dnsHost.setIPAddressesAsStrings(serverAddresses);
-
             dnsHostsList.add(dnsHost);
         }
-
         return dnsHostsList;
     }
 
-    public void setRootServers(List<DNSHost> rootServers) {
-        this.rootServers = rootServers;
+    public List<Parameter> getConfig(List<DNSHost> rootServers) throws ConfigException {
+        List<Parameter> ret = new ArrayList<Parameter>();
+        for (DNSHost host : rootServers) {
+            Parameter param = new SetParameter(rootServerParamNames + "." + host.getName(), host.getIPAddressesAsStrings());
+            ret.add(param);
+        }
+        return ret;
     }
+
 }
